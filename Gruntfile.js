8ddfe94c410aa943e2394bfa01c8bc2cf434a6e4
m-idler <marcel.idler@mayflower.de>
2015-11-11 14:30:06 +0100

    move sass config to a single file in config directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,4 @@
 module.exports = function(grunt) {
-    var DEBUG = grunt.option('dbg');
-
     var moduleName = "ui";
     var loadConfig = function (name, module) {
         var result = {};
@@ -14,24 +12,7 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON("package.json"),
         webpack: loadConfig("webpack"),
         uglify: loadConfig("uglify"),
-        sass: {
-            dist: {
-                options: {
-                    outputStyle: DEBUG ? 'expanded' : 'compressed',
-                    sourceMap: DEBUG,
-                    sourceMapEmbed: true
-                    /*
-                     includePaths: [
-                     'bower_components/susy/sass'
-                     ]
-                     */
-                },
-                files: {
-                    'dist/showcar-ui.css': 'scss/showcar-ui.scss',
-                    'docs/css/documenation.css': 'docs/css/documenation.scss'
-                }
-            }
-        },
+        sass: loadConfig("sass"),
         watch: {
             files: ['scss/**/*.scss', 'js/**/*.js','docs/index.html', 'docs/css/*.scss'],
             tasks: ['sass', "webpack", "uglify"],
@@ -50,3 +31,4 @@ module.exports = function(grunt) {
         pattern: ['grunt-*', "!grunt-cli"]
     });
 };
+
---
+++ config/sass.conf.js
@@ -0,0 +1,21 @@
+module.exports = (function() {
+    var grunt = require("grunt");
+    var DEBUG = !!grunt.option('dbg');
+
+    return {
+        options: {
+            outputStyle: DEBUG ? 'expanded' : 'compressed',
+            sourceMap: DEBUG,
+            sourceMapEmbed: true
+            /*
+             includePaths: [
+             'bower_components/susy/sass'
+             ]
+             */
+        },
+        files: {
+            'dist/showcar-ui.css': 'scss/showcar-ui.scss',
+            'docs/css/documenation.css': 'docs/css/documenation.scss'
+        }
+    }
+})();
