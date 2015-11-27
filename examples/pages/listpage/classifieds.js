6a3d38a76e4fb660615b726f38a1e48a38eb2ff7
sspringer <sebastian.springer@mayflower.de>
2015-11-27 10:10:45 +0100

    added listpage example

---
+++ examples/pages/listpage/classifieds.js
@@ -0,0 +1,54 @@
+var showcarDemoData = showcarDemoData || {};
+
+showcarDemoData["classifieds"] = [
+    {
+        url: "/offers/opel-corsa-joy-gasoline-blue-279328991",
+        thumbnail: "http://pic.autoscout24.net/images/991/328/0279328991001.jpg?c9bbc4237c94154ab66c6b0a2ba48a4a",
+        name: "Opel Corsa Joy",
+        price: "€ 1,-",
+        mileage: "194,360 km",
+        power: "44 kW (60 HP)",
+        firstRegistration: "01/1994",
+        envkv: "Used, Gasoline"
+    },
+    {
+        url: "/offers/jaguar-xj-gasoline-blue-279799115",
+        thumbnail: "http://pic.autoscout24.net/images/115/799/0279799115001.jpg?1c370be7e32ea649687459de219ce8eb",
+        name: "Jaguar XJ",
+        price: "€ 1,-",
+        mileage: "90,000 km",
+        power: "186 kW (253 HP)",
+        firstRegistration: "07/1978",
+        envkv: "Used, Gasoline"
+    },
+    {
+        url: "/offers/abarth-695-14-turbo-t-jet-biposto-gasoline-grey-279737105",
+        thumbnail: "http://pic.autoscout24.net/images/105/737/0279737105001.jpg?ee18cb21b7695602fe0847c64f658074",
+        name: "Abarth 695 1.4 Turbo T-Jet biposto",
+        price: "€ 1,-",
+        mileage: "0 km",
+        power: "140 kW (190 HP)",
+        firstRegistration: "-/-",
+        envkv: "New, Gasoline, Consumption combined 6.5l/100 km*, Co2 Emission combined 150 g/km*"
+    },
+    {
+        url: "/offers/alfa-romeo-giulietta-20-jtdm-2-150-cv-distinctive-diesel-white-280009043",
+        thumbnail: "http://pic.autoscout24.net/images/043/009/0280009043001.jpg?b3c2edfbcdbac99b09aad3d16207e362",
+        name: "Alfa Romeo Giulietta 2.0 JTDm-2 150 CV Distinctive",
+        price: "€ 1,-",
+        mileage: "0 km",
+        power: "110 kW (150 HP)",
+        firstRegistration: "-/-",
+        envkv: "New, Diesel, Consumption combined 4.2l/100 km*, Co2 Emission combined 110 g/km*"
+    },
+    {
+        url: "/offers/renault-alpine-v6-28-v6-gt-gasoline-black-279953396",
+        thumbnail: "http://pic.autoscout24.net/images/396/953/0279953396001.jpg?fc5b99241862d9785dd7928f4199a727",
+        name: "Renault Alpine V6 2.8 V6 GT",
+        price: "€ 1,-",
+        mileage: "190,000 km",
+        power: "116 kW (158 HP)",
+        firstRegistration: "03/1986",
+        envkv: "Used, Gasoline"
+    }
+];
