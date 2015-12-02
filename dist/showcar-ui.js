691c628d6c2069e5af64a4db585fc02a02ae2f22
Markus Handschuh <yuridesigns@gmail.com>
2015-12-02 14:44:47 +0100

    copy picturefill.min.js to dist/polyfills

--- dist/showcar-ui.js
+++ dist/showcar-ui.js
@@ -2250,50 +2250,134 @@
 	    cookie: __webpack_require__(13)
 	};
 	
-	module.exports = (function () {
+	var Storage = (function () {
+	    /**
+	     * @constructor
+	     * @param {string} type The store backend to use
+	     * @param {boolean} [silent=false] Whether to throw exceptions or fail silently returning false
+	     */
+	
 	    function Storage(type) {
+	        var _ref = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+	
+	        var _ref$silent = _ref.silent;
+	        var silent = _ref$silent === undefined ? false : _ref$silent;
+	
 	        _classCallCheck(this, Storage);
 	
 	        if (!(type in stores)) {
-	            throw new Error('Unsupported type ' + type);
+	            this.fail('Storage: Unsupported type ' + type);
 	        }
 	
+	        this.silent = !!silent;
 	        this.store = new stores[type]();
 	    }
+	
+	    /**
+	     * Gets the stored value for a specified key
+	     * @param {string} key The key to look up
+	     * @param defaultValue Return this if no value has been found
+	     * @throws {Error} If not silent
+	     * @returns {string} The stored value or defaultValue
+	     */
 	
 	    _createClass(Storage, [{
 	        key: 'get',
 	        value: function get(key) {
 	            var defaultValue = arguments.length <= 1 || arguments[1] === undefined ? null : arguments[1];
 	
-	            var result = this.store.get(key);
+	            try {
+	                var result = this.store.get(key);
 	
-	            if (null === result) {
-	                return defaultValue;
+	                if (null === result) {
+	                    return defaultValue;
+	                }
+	                return result;
+	            } catch (e) {
+	                return this.fail(e);
 	            }
-	            return result;
 	        }
+	
+	        /**
+	         * Writes a value to the store under the specified key
+	         * @param {string} key The key to use when storing
+	         * @param {string} value The value to store
+	         * @param {object} [options] A map of options. See the respective backends.
+	         * @throws {Error} If not silent
+	         * @returns {Storage|boolean} If silent, returns false on error. Returns this on success.
+	         */
+	
 	    }, {
 	        key: 'set',
 	        value: function set(key, value, options) {
-	            this.store.set(key, value, options);
-	            return this;
+	            try {
+	                this.store.set(key, value, options);
+	                return this;
+	            } catch (e) {
+	                return this.fail(e);
+	            }
 	        }
+	
+	        /**
+	         * Checks whether the store knows about the specified key
+	         * @param {string} key The key to check for existance
+	         * @throws {Error} If not silent
+	         * @returns {boolean} If silent, returns false on error (!!)
+	         */
+	
 	    }, {
 	        key: 'has',
 	        value: function has(key) {
-	            return this.store.has(key);
+	            try {
+	                return this.store.has(key);
+	            } catch (e) {
+	                return this.fail(e);
+	            }
 	        }
+	
+	        /**
+	         * Deletes the specified key and its value from the store
+	         * @param {string} key The key to delete
+	         * @returns {Storage|boolean} If silent, returns false on error
+	         */
+	
 	    }, {
 	        key: 'remove',
 	        value: function remove(key) {
-	            this.store.remove(key);
-	            return this;
+	            try {
+	                this.store.remove(key);
+	                return this;
+	            } catch (e) {
+	                return this.fail(e);
+	            }
+	        }
+	
+	        /**
+	         * Wrapper for error handling
+	         * @private
+	         * @param {Error|string} reason What is happening?
+	         * @returns {boolean}
+	         */
+	
+	    }, {
+	        key: 'fail',
+	        value: function fail(reason) {
+	            if (this.silent) {
+	                return false;
+	            }
+	
+	            if (!reason instanceof Error) {
+	                reason = new Error(reason);
+	            }
+	
+	            throw reason;
 	        }
 	    }]);
 	
 	    return Storage;
 	})();
+	
+	module.exports = Storage;
 
 /***/ },
 /* 11 */
