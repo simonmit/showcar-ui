457aca32b33e847be3fa101a9737f6f6580f19b4
Florian Fackler <florian.fackler@mintao.com>
2016-01-19 17:07:07 +0100

    Pager fixes

--- docs/js/pager.js
+++ docs/js/pager.js
@@ -1,8 +1,10 @@
-$(function(){
-    var pages = $('.sc-pagination li').not('.next-page').find('a');
+!function($) {
+    $(function(){
+        var pages = $('.sc-pagination li').not('.next-page').find('a');
 
-    pages.on('click', function(e) {
-        pages.removeClass('active');
-        $(e.target).addClass('active');
+        pages.on('click', function(e) {
+            pages.removeClass('active');
+            $(e.target).addClass('active');
+        });
     });
-});
+}(window.Zepto);
