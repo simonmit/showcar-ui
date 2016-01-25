665f2fe1da535079bb51eea60ccf35fc693a5ac4
Florian Fackler <florian.fackler@mintao.com>
2016-01-25 11:14:52 +0100

    Removed locally hosted polyfills from grunt task

--- config/copy.conf.js
+++ config/copy.conf.js
@@ -1,9 +1,5 @@
 module.exports = {
     files: [
-        {flatten: true, expand: true, src: 'vendor/dom4/build/dom4.js', dest: 'dist/polyfills'},
-        {flatten: true, expand: true, src: 'vendor/es5-shim/es5-shim.min.js', dest: 'dist/polyfills'},
-        {flatten: true, expand: true, src: 'vendor/placeholders/dist/placeholders.min.js', dest: 'dist/polyfills'},
-        {flatten: true, expand: true, src: 'vendor/picturefill/dist/picturefill.min.js', dest: 'dist/polyfills'},
         {flatten: true, expand: true, src: 'vendor/source-sans-pro/WOFF2/OTF/SourceSansPro-Light.otf.woff2', dest: 'dist/fonts'},
         {flatten: true, expand: true, src: 'vendor/source-sans-pro/WOFF2/OTF/SourceSansPro-Regular.otf.woff2', dest: 'dist/fonts'},
         {flatten: true, expand: true, src: 'vendor/source-sans-pro/WOFF2/OTF/SourceSansPro-Semibold.otf.woff2', dest: 'dist/fonts'},
