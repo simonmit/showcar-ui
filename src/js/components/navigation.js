1c9e9b00157b9db57faa676bd55f87f580aa531b
Markus Handschuh <yuridesigns@gmail.com>
2016-01-26 15:35:15 +0100

    add navigation

--- src/js/components/navigation.js
+++ src/js/components/navigation.js
@@ -1,94 +1,82 @@
 module.exports = function () {
 
-    var navigation = document.querySelector('nav');
+    var toggleMenu = function(menuItem, navigationNode)  {
+        var openElement;
 
-    if (navigation) {
+        var titleElm = menuItem.querySelector('span.title');
+        if(titleElm) {
+            openElement = navigationNode.querySelector('.open');
+            if(openElement) {
+                openElement.classList.remove('open');
+            }
 
-        navigation.querySelector('.header-trigger').addEventListener('click', function () {
-            navigation.classList.toggle('open');
+            if(menuItem !== openElement) {
+                menuItem.classList.add('open');
+            }
+        }
+    };
+
+    var showMenu = function(e, navigationNode) {
+        var elm = e.elm;
+        var openElement;
+        var selectedLink;
+
+        if (elm && elm.nodeName.toLowerCase() === 'span') {
+            if(elm.classList.contains('title')) {
+                openElement = navigationNode.querySelector('.open');
+                if(openElement) {
+                    selectedLink = openElement.querySelector('span.title');
+                }
+
+                if(elm !== selectedLink) {
+                    elm.parentNode.classList.add('open');
+                }
+                e.stopPropagation();
+            }
+        }
+    };
+
+    var getPageName = function() {
+        if(dataLayer) {
+            for(var i=0;dataLayer.length;i++) {
+                if(dataLayer[i].common_pageName) {
+                    return dataLayer[i].common_pageName;
+                }
+            }
+            return '';
+        }
+    };
+
+    Array.prototype.forEach.call(document.querySelectorAll('.sc-navigation'), function (node) {
+        if (node) {
+            setupNavigation(node);
+        }
+    });
+
+    function setupNavigation(node) {
+        node.querySelector('.sc-btn-mobile-menu').addEventListener('click', function () {
+            node.classList.toggle('open');
         });
 
-        navigation.addEventListener('click', function (e) {
-            var elm = e.target || e.srcElement;
+        node.addEventListener('click', function (e) {
+            var elm = e.elm || e.srcElement;
 
             if (elm.nodeName.toLowerCase() === 'li') {
-                toggleMenu(elm);
+                toggleMenu(elm, node);
                 e.stopPropagation();
-            } else if (elm.nodeName.toLowerCase() === 'span' || elm.classList.contains('subnav-title')) {
-                toggleMenu(elm.parentNode);
+            } else if (elm.nodeName.toLowerCase() === 'span' || elm.classList.contains('title')) {
+                toggleMenu(elm.parentNode, node);
                 e.stopPropagation();
             }
         });
 
-        var toggleMenu = function(menuItem)  {
-            var openElement;
-
-            var titleElm = menuItem.querySelector('span.subnav-title');
-            if(titleElm) {
-                openElement = navigation.querySelector('.open');
-                if(openElement) {
-                    openElement.classList.remove('open');
-                }
-
-                if(menuItem !== openElement) {
-                    menuItem.classList.add('open');
-                }
-            }
-        };
-
-        var showMenu = function(e) {
-            var elm = e.target;
-            var openElement;
-            var selectedLink;
-
-            if (elm && elm.nodeName.toLowerCase() === 'span') {
-                if(elm.classList.contains('subnav-title')) {
-                    openElement = navigation.querySelector('.open');
-                    if(openElement) {
-                        selectedLink = openElement.querySelector('span.subnav-title');
-                    }
-
-                    if(elm !== selectedLink) {
-                        elm.parentNode.classList.add('open');
-                    }
-                    e.stopPropagation();
-                }
-            }
-        };
-
-        navigation.addEventListener('keydown', function(e) {
-            if (e.keyCode == 13) toggleMenu(e); //enter key
-            if (e.keyCode == 9) showMenu(e); //tab key
+        node.addEventListener('keydown', function(e) {
+            if (e.keyCode == 13) toggleMenu(e, node); //enter key
+            if (e.keyCode == 9) showMenu(e, node); //tab key
         });
-
-        navigation.addEventListener('click', function(e) {
-            var target = e.target || e.srcElement;
-            if (target && target.nodeName.toLowerCase() === 'a'){
-                if (target.href){
-                    var pageName = getPageName();
-                    if(pageName.length > 0) {
-                        var appendChar = (target.href.indexOf('?') > -1) ? '&' : '?';
-                        window.location.href = target.href + appendChar + "genlnk=navi&genlnkorigin=" + pageName;
-                        e.stopPropagation();
-                        e.preventDefault();
-                    }
-                }
-            }
-        });
-
-        var getPageName = function() {
-            if(dataLayer) {
-                for(var i=0;dataLayer.length;i++) {
-                    if(dataLayer[i].common_pageName) {
-                        return dataLayer[i].common_pageName;
-                    }
-                }
-                return '';
-            }
-        };
 
         document.body.addEventListener('click', function(e) {
-            var openElement = navigation.querySelector('.open');
+            var openElement = node.querySelector('.open');
             if(openElement) {
                 openElement.classList.remove('open');
             }
