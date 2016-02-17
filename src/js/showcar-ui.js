8f62f0a3487635e832e9cc2c9ba015f62694fc0b
m-idler <marcel.idler@mayflower.de>
2016-02-17 18:08:38 +0100

    fix bug when checking if custom elements are already registered reorder main js code by priority

--- src/js/showcar-ui.js
+++ src/js/showcar-ui.js
@@ -1,48 +1,8 @@
 require('../../vendor/zepto/zepto.min.js');
-require('./components/rotating-arrow.js')();
-require('./components/sticky.js')();
-require('./components/navigation.js');
 require('./components/polyfills.js')();
-require('./components/collapse.js')();
-
-window.notification = require('./components/notification.js');
 
 var FontFaceObserver = require('fontfaceobserver');
 var observer = new FontFaceObserver('Source Sans Pro');
-var warn = function (msg) {
-    if (!window || !window.console) {
-        return;
-    }
-    window.console.warn(msg);
-};
-var isRegistered = function(name) {
-    return document.createElement(name).constructor !== HTMLElement;
-};
-
-if (!isRegistered('as24-icon')) {
-    require('../../vendor/showcar-icons/dist/showcar-icons.min.js');
-} else {
-    warn('as24-icon is already registered.');
-}
-
-if (!isRegistered('custom-dropdown')) {
-    require('./components/custom-dropdown.js');
-} else {
-    warn('custom-dropdown is already registered.');
-}
-
-if (!window.storage) {
-    window.Storage = require('../../vendor/showcar-storage/src/storage.js');
-} else {
-    warn('window.storage is already registered.');
-}
-
-if (!window.notification) {
-    window.notification = require('./components/notification.js');
-} else {
-    warn('window.notification is already registered.');
-}
-
 try {
     observer.check().then(function () {
         $('body').addClass('font-loaded');
@@ -51,6 +11,46 @@ try {
     });
 } catch (e) {
     warn('Failed to use FontFaceObserver', e);
+}
+
+var warn = function (msg) {
+    if (!window || !window.console) {
+        return;
+    }
+    window.console.warn(msg);
+};
+
+var isRegistered = function(name) {
+    var registered = document.createElement(name).constructor !== HTMLElement;
+    if (registered) {
+        warn('CustomElement "' + name + '" is already registered.');
+    }
+    return registered;
+};
+
+if (!window.storage) {
+    window.Storage = require('../../vendor/showcar-storage/src/storage.js');
+} else {
+    warn('window.storage is already registered.');
+}
+
+if (!isRegistered('as24-icon')) {
+    require('../../vendor/showcar-icons/dist/showcar-icons.min.js');
+}
+
+if (!isRegistered('custom-dropdown')) {
+    require('./components/custom-dropdown.js');
+}
+
+require('./components/navigation.js');
+require('./components/rotating-arrow.js')();
+require('./components/sticky.js')();
+require('./components/collapse.js')();
+
+if (!window.notification) {
+    window.notification = require('./components/notification.js');
+} else {
+    warn('window.notification is already registered.');
 }
 
 
