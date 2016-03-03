1cccb061165ddf034568955f6050aadb7dfe5034
Markus Handschuh <yuridesigns@gmail.com>
2016-03-03 10:17:20 +0100

    style changes

--- src/js/components/rotating-arrow.js
+++ src/js/components/rotating-arrow.js
@@ -1,4 +1,4 @@
-module.exports = function() {
+module.exports = () => {
     Array.prototype.forEach.call(document.querySelectorAll('[data-toggle="arrow"]'), (arrow) => {
         let arrowEl = $(arrow);
         $(arrowEl.data('target')).on('click', () => arrowEl.toggleClass('open'));
