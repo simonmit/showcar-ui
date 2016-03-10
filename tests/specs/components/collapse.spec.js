b7d8a9b477f00be92dfa0b1dcbd09ba0bdaec49b
micha trieba <micha.trieba@mayflower.de>
2016-03-10 17:21:12 +0100

    removede sample code. We have 2 working specs for remote test

--- tests/specs/components/collapse.spec.js
+++ tests/specs/components/collapse.spec.js
@@ -1,11 +1,4 @@
-var chai        = require('chai'),
-    assert      = chai.assert,
-    webdriverio = require('webdriverio');
-
 describe('Components collapse tests', function(){
-
-    this.timeout(30000);
-    var client = {};
 
     beforeEach(function() {
         console.log('beforeEach');
@@ -18,13 +11,13 @@ describe('Components collapse tests', function(){
         browser.isExisting('#collapse-target-1.in').should.be.false;
         browser.isVisible('[data-target="#collapse-target-1"]').should.be.true;
 
-        browser.click('[data-target="#collapse-target-1"]');
-        browser.isVisible('[data-target="#collapse-target-1"]').should.be.true;
+        browser.click('[data-target="#collapse-target-1"]')
+            .isVisible('[data-target="#collapse-target-1"]').should.be.true;
         browser.isExisting('#collapse-target-1.in').should.be.true;
         browser.isVisible('#collapse-target-1').should.be.true;
 
-        browser.click('[data-target="#collapse-target-1"]');
-        browser.isVisible('[data-target="#collapse-target-1"]').should.be.true;
+        browser.click('[data-target="#collapse-target-1"]')
+            .isVisible('[data-target="#collapse-target-1"]').should.be.true;
         browser.isExisting('#collapse-target-1.in').should.be.false;
         browser.isVisible('#collapse-target-1').should.be.false;
 
@@ -34,30 +27,15 @@ describe('Components collapse tests', function(){
 
     it('collapse test more/less example',function () {
 
-        /*
-        return browser
-            .click('[data-test="example-collapse-show"]')
-            .isVisible('[data-test="example-collapse-show"]').should.eventually.be.false
-            .getText('[data-test="example-collapse-show"]').should.eventually.be.equal('Show content')
-        ;
-        */
-
-        //browser.isVisible('[data-test=example-collapse-show]').should.be.true;
-        /*
-        return browser.isVisible('[data-test="example-collapse-show"]').then(function(visible) {
-            visible.should.be.true;
-        });
-        */
-        browser.isVisible('[data-test="example-collapse-show"]').should.be.true;
-        browser.isVisible('.sc-collapse-target:not([data-test="example-collapse-show"])').should.be.all.false;
-
+        browser.isVisible('#collapse-target-1').should.be.false;
+        browser
+            .click('[data-target="#collapse-target-1"]')
+            .isVisible('#collapse-target-1').should.be.true;
 
         return browser;
-        //browser.call(done)
     });
 
+    it('should have a submenu after click on the main menu link', function () {
 
-    after(function(done) {
-        //browser.end(done);
     });
 });
