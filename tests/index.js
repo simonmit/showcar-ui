02da7fb0404579f4dd5504eba6d4af2a5a95ae0e
gvand <account@gvand.net>
2016-05-13 11:11:47 +0200

    changed the mocha test back to parallel

--- tests/index.js
+++ tests/index.js
@@ -12,17 +12,21 @@ const checkForJsErrors = require('./sanity/checkForJsErrors');
 const pageids = ['home', 'list', 'detail'];
 const locales = argv.locale ? [argv.locale] : ['de-DE','de-AT','en-GB'];
 
-locales.forEach(locale => {
-    pageids.forEach(pageid => {
-        const config = getPageConfig(locale, pageid);
-        if (!config) { return; }
-        let url = config.url;
-        if(process.env.CI_BUILD_REF_NAME === 'develop'){
-            url = `${url}?sc_develop_ver=${process.env.CI_BUILD_REF}`;
-        } else if (process.env.CI_BUILD_REF_NAME === 'master'){
-            url = `${url}?sc_master_ver=${process.env.CI_BUILD_REF}`;
-        }
-        it(`URL must return 200 OK (${locale}; ${pageid})`, check200(url));
-        it(`There must not be any JS error on the page (${locale}; ${pageid})`, checkForJsErrors(url));
+const parallel = require('mocha.parallel');
+
+parallel('Checking pages', function() {
+    locales.forEach(locale => {
+        pageids.forEach(pageid => {
+            const config = getPageConfig(locale, pageid);
+            if (!config) { return; }
+            let url = config.url;
+            if(process.env.CI_BUILD_REF_NAME === 'develop'){
+                url = `${url}?sc_develop_ver=${process.env.CI_BUILD_REF}`;
+            } else if (process.env.CI_BUILD_REF_NAME === 'master'){
+                url = `${url}?sc_master_ver=${process.env.CI_BUILD_REF}`;
+            }
+            it(`URL must return 200 OK (${locale}; ${pageid})`, check200(url));
+            it(`There must not be any JS error on the page (${locale}; ${pageid})`, checkForJsErrors(url));
+        });
     });
 });
