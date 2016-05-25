f97c3f7fbdd303f31c9f977eb83adbb3dddeffc7
gvand <account@gvand.net>
2016-05-25 17:07:02 +0200

    sticky button fix

--- src/js/components/sticky.js
+++ src/js/components/sticky.js
@@ -1,6 +1,26 @@
 module.exports = () => {
+
+    function detectIE() {
+        var ua = window.navigator.userAgent;
+        var msie = ua.indexOf('MSIE ');
+        if (msie > 0) {
+            return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+        }
+        var trident = ua.indexOf('Trident/');
+        if (trident > 0) {
+            var rv = ua.indexOf('rv:');
+            return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        }
+        var edge = ua.indexOf('Edge/');
+        if (edge > 0) {
+            return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+        }
+        return false;
+    }
+
     function handleStickies() {
-        let scrollPos = $(window).scrollTop();
+        
+        let scrollPos = detectIE() ? document.documentElement.scrollTop : $(window).scrollTop();
 
         Array.prototype.forEach.call($('[data-sticky]'), (stickyButton) => {
             let stickyEl = $(stickyButton);
