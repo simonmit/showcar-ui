cefe1b777cd3492e8ae1db8c5f36fe1fd6967d08
gvand <account@gvand.net>
2016-05-31 19:28:31 +0200

    refactored the sticky component

--- src/js/components/sticky.js
+++ src/js/components/sticky.js
@@ -1,78 +1,121 @@
 module.exports = () => {
 
-    function detectIE() {
-        var ua = window.navigator.userAgent;
-        var msie = ua.indexOf('MSIE ');
-        if (msie > 0) {
-            return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    /**
+     * add an class to the given dom element
+     * ToDo: move it to utils or use a polyfill
+     * @param {string} className
+     * @param {HTMLElement} domEl
+     * @returns {HTMLElement}
+     */
+    function addClass(className, domEl) {
+        let classList = [], classesString = domEl.getAttribute('class');
+        if (classesString) {
+            classList = classesString.split(' ');
+            if (classList.indexOf(className) === -1) {
+                classesString = classList.concat(className).join(' ');
+            }
+        } else {
+            classesString = className
         }
-        var trident = ua.indexOf('Trident/');
-        if (trident > 0) {
-            var rv = ua.indexOf('rv:');
-            return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-        }
-        var edge = ua.indexOf('Edge/');
-        if (edge > 0) {
-            return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-        }
-        return false;
+        domEl.setAttribute('class', classesString);
+        return domEl;
     }
 
+    /**
+     * remove an class to the given dom element
+     * ToDo: move it to utils or use a polyfill
+     * @param {string} className
+     * @param {HTMLElement} domEl
+     * @returns {HTMLElement}
+     */
+    function removeClass(className, domEl) {
+        let classList = [], classesString = domEl.getAttribute('class');
+        if (classesString) {
+            classList = classesString.split(' ');
+            if(classList.indexOf(className) !== -1){
+                classList.splice(classList.indexOf(className), 1);
+            }
+            domEl.setAttribute('class', classList.join(' '));
+        }
+        return domEl;
+    }
+
+    /**
+     * gets the current document height.
+     * @returns {number}
+     */
+    function getDocumentHeight() {
+        return Math.max(
+            document.body.scrollHeight, document.documentElement.scrollHeight,
+            document.body.offsetHeight, document.documentElement.offsetHeight,
+            document.body.clientHeight, document.documentElement.clientHeight
+        );
+    }
+
+    /**
+     * gets the current client height.
+     * @returns {number}
+     */
+    function getWindowHeight() {
+        return window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    }
+
+    /**
+     * handler for positioning the sticky elements.
+     */
     function handleStickies() {
-        
-        let scrollPos = detectIE() ? document.documentElement.scrollTop : $(window).scrollTop();
 
-        Array.prototype.forEach.call($('[data-sticky]'), (stickyButton) => {
-            let stickyEl = $(stickyButton);
-            let id = stickyEl.attr('data-sticky');
-            let undockEl = $('[data-sticky-undock="' + id + '"]');
-            let dockEl = $('[data-sticky-dock="' + id + '"]');
+        // get some basic values element like: scroll position, document and window height and the sticky elements
+        let scrollPosition  = document.body.scrollTop || document.documentElement.scrollTop;
+        let stickies        = document.querySelectorAll('[data-sticky]');
+        let bodyOffset      = document.body.getBoundingClientRect().top;
+        let documentHeight  = getDocumentHeight();
+        let windowHeight    = getWindowHeight();
 
-            // if there is no dock and undock element leave sticky class
-            if (undockEl.length === 0 && dockEl.length === 0) {
-                stickyEl.addClass('sticky');
+        [].forEach.call(stickies, stickyElement => {
+
+            // Get the dock and un-dock element by the id of sticky
+            let id              = stickyElement.getAttribute('data-sticky');
+            let undockElement   = document.querySelector(`[data-sticky-undock="${id}"]`);
+            let dockElement     = document.querySelector(`[data-sticky-dock="${id}"]`);
+
+            // If device height is to small don't stick!
+            if(windowHeight < 320){
+                removeClass('sc-sticky', stickyElement);
+                return;
             }
 
-            // get undock position
-            let undockPos = 0;
-            if (undockEl.length > 0) {
-                undockPos = undockEl.offset().top;
+            // If there is no dock or un-dock element just stick!
+            if (!undockElement && !dockElement) {
+                addClass('sc-sticky', stickyElement);
+                return;
             }
 
-            // get dock position
-            let dockPos = $(document).height();
-            if(dockEl.length > 0) {
-                dockPos = dockEl.offset().top;
-            }
+            // Get dock and un-dock element positions
+            let undockPosition  = undockElement ? undockElement.getBoundingClientRect().top - bodyOffset : 0;
+            let dockPosition    = dockElement ? dockElement.getBoundingClientRect().top - bodyOffset : documentHeight;
 
-            // if element is within scrolling area, scroll, else don't
-            if (scrollPos + $(window).height() > undockPos && scrollPos < dockPos - $(window).height() + stickyEl.height() * 1.5) {
-                stickyEl.addClass('sc-sticky');
+            // Decide based on the position to stick or not stick
+            if (scrollPosition + windowHeight > undockPosition &&
+                scrollPosition < dockPosition - windowHeight + stickyElement.getBoundingClientRect().height * 1.5) {
+                addClass('sc-sticky', stickyElement);
             } else {
-                stickyEl.removeClass('sc-sticky');
+                removeClass('sc-sticky', stickyElement);
             }
         });
     }
 
+    /**
+     * initial positioning of the sticky
+     */
     handleStickies();
 
-    window.addEventListener("deviceorientation", () => {
-        handleStickies();
-    });
-
-    window.addEventListener("resize", () => {
-        handleStickies();
-    });
-
-    window.addEventListener("pageSizeChanged", () => {
-        handleStickies();
-    });
-
-    document.addEventListener('scroll', () => {
-        handleStickies();
-    });
-
-    document.addEventListener('collapse', () => {
-        handleStickies();
-    });
+    /**
+     * adding several event listeners needed for updating the positions of the stickies
+     */
+    window.addEventListener("deviceorientation", () => handleStickies());
+    window.addEventListener("resize", () => handleStickies());
+    window.addEventListener("pageSizeChanged", () => handleStickies());
+    document.addEventListener('scroll', () => handleStickies());
+    document.addEventListener('collapse', () => handleStickies());
 };
