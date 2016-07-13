4013a0a507926877475e0fd6daf40475a15a4279
ctoutountzidis <ctoutountzidis@autoscout24.com>
2016-07-13 10:08:26 +0200

    Try to fix come css tests by setting domain along with sample visitor cookie.

--- tests/css/scripts/beforeScript.js
+++ tests/css/scripts/beforeScript.js
@@ -4,7 +4,8 @@ module.exports = function(casper, scenario, vp) {
         casper.page.addCookie({
             'name': 'as24Visitor',
             'value': 'f87cc2b2-41d1-4ef9-bea3-df9bc1fcd996',
-            'path': '/'
+            'path': '/',
+            'domain': '.autoscout24.com'
         });
     });
 };
