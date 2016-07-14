a1975c5d748431798413bfca4589ec0a228aeafc
ctoutountzidis <ctoutountzidis@autoscout24.com>
2016-07-14 15:07:59 +0200

    Added event for reinitializing collapse events.

--- src/js/components/collapse.js
+++ src/js/components/collapse.js
@@ -1,17 +1,22 @@
 module.exports = () => {
-    Array.prototype.forEach.call(document.querySelectorAll('[data-toggle="sc-collapse"]'), (collapsable) => {
-        collapsable.onclick = () => {
-            let targetAttr = collapsable.getAttribute('data-target');
-            let targets = document.querySelectorAll(targetAttr);
+    function init() {
+        Array.prototype.forEach.call(document.querySelectorAll('[data-toggle="sc-collapse"]'), (collapsable) => {
+            collapsable.onclick = () => {
+                let targetAttr = collapsable.getAttribute('data-target');
+                let targets = document.querySelectorAll(targetAttr);
 
-            Array.prototype.forEach.call(targets, (target) => {
-                target.classList.toggle('in');
-            });
+                Array.prototype.forEach.call(targets, (target) => {
+                    target.classList.toggle('in');
+                });
 
-            let event = new CustomEvent('collapse', {
-                bubbles: true
-            });
-            document.dispatchEvent(event);
-        }
-    });
+                let event = new CustomEvent('collapse', {
+                    bubbles: true
+                });
+                document.dispatchEvent(event);
+            }
+        });
+    };
+    
+    init();
+    document.addEventListener('as24-collapse:update', init);
 };
