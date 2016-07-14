7458530dc8f26f46599a27c85b97fcb2983417b1
Ivan Demchenko <idemchenko@autoscout24.com>
2016-07-14 12:54:03 +0200

    Fixed build setup (grunt) disabled css tests
    Only sanity tests are being enabled

---
+++ config/sass.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt, options) {
+    var DEBUG = !!options.debug;
+
+    return {
+        options: {
+            outputStyle: DEBUG ? 'expanded' : 'compressed',
+            importer: function(url, prev, done) {
+                return done(url.split(':')[0].toLowerCase() === 'npm'
+                    ? { file: require.resolve(url.split(':')[1]) }
+                    : null);
+            }
+        },
+        dist: {
+            files: { 'dist/showcar-ui.css': 'src/scss/showcar-ui.scss' }
+        },
+        docs: {
+            files: [
+                { dest: 'docs/lib/showcar-ui.css', src: 'src/scss/showcar-ui.scss' },
+                { dest: 'docs/lib/css/documentation.css', src: 'src/docs/css/documentation.scss' },
+                {
+                    expand: true,
+                    cwd: '/src/docs/examples/',
+                    src: ['**/*.scss'],
+                    dest: './docs/examples',
+                    ext: '.css'
+                }
+            ]
+        }
+    }
+};
