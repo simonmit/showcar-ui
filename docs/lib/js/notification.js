f69a48fe3e90b0cc0235b0571c463f367e35f8f7
Ivan Demchenko <idemchenko@autoscout24.com>
2016-07-14 14:33:22 +0200

    Removed old docs

--- docs/lib/js/notification.js
+++
@@ -1,8 +0,0 @@
-!function($) {
-    $('.notification-demo').click(function(event) {
-        var id = this.getAttribute('data-id');
-        var el = document.querySelector('#' + id);
-
-        el.classList.toggle('show');
-    });
-}(window.Zepto);
