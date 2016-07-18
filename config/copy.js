1ecf3eec52801d3e5bdd467162d0ad47169b88fc
meckert <mathias.eckert@autoscout24.com>
2016-07-18 14:07:01 +0200

    copying docs files to public folder since public folder is default for gitlab pages

--- config/copy.js
+++ config/copy.js
@@ -2,7 +2,8 @@ module.exports = {
     docs: {
         files: [
             {expand: true, cwd: './src/docs/', src: ['css/*', 'js/*'], dest: './docs/lib/'},
-            {expand: true, cwd: './src/docs/examples/', src: ['**/*'], dest: './docs/examples'}
+            {expand: true, cwd: './src/docs/examples/', src: ['**/*'], dest: './docs/examples'},
+            {expand: true, cwd: './docs/', src: ['**/*'], dest: './public'}
         ]
     }
 };
