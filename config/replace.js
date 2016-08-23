a945fbc8e2fa0e6255545d5c10c153a6ae13eb67
Adam <aberesdeak@autoscout24.com>
2016-08-23 17:29:50 +0200

    inlining almost all polyfills to make Google Page Speed Insights happy + using smaller and simpler polyfills for Array.of, Array.from and Object.assign methods

--- config/replace.js
+++ config/replace.js
@@ -10,8 +10,15 @@ module.exports = function(grunt, options) {
             options:{
                 patterns: [
                     { match: 'ASSET_PATH', replacement: '<%= assetsPrefix %>/<%= buildType %>/<%= commitHash %>' },
+
                     { match: 'POLYFILL_DOCUMENT_REGISTER_ELEMENT', replacement: readFile('node_modules/document-register-element/build/document-register-element.js') },
                     { match: 'POLYFILL_DOM4', replacement: readFile('node_modules/dom4/build/dom4.js') },
+                    { match: 'POLYFILL_ARRAY', replacement: readJsFile('src/js/polyfills/array.js') },
+                    { match: 'POLYFILL_OBJECT', replacement: readJsFile('src/js/polyfills/object.js') },
+                    { match: 'POLYFILL_PROMISE', replacement: readJsFile('node_modules/promiz/promiz.min.js') },
+                    { match: 'POLYFILL_FETCH', replacement: readJsFile('node_modules/whatwg-fetch/fetch.js') },
+                    { match: 'POLYFILL_ES6_COLLECTIONS', replacement: readJsFile('node_modules/es6-collections/es6-collections.js') },
+
                     { match: 'SCRIPT_ERROR_COLLECTOR', replacement: readJsFile('src/js/inline-js/js-error-collector.js') },
                     { match: 'SCRIPT_FONT_LOADER', replacement: readJsFile('src/js/inline-js/font-loader.js') },
                 ],
