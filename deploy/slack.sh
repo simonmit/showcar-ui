b4f4ed2dee5bd1e3fcaabe9a41553844a48c8bad
Adam <aberesdeak@autoscout24.com>
2016-12-02 14:47:29 +0100

    trying to fix notification build step

--- deploy/slack.sh
+++ deploy/slack.sh
@@ -1,1 +1,2 @@
-curl -X POST --data-urlencode 'payload={"channel": "#as24_web_experience", "username": "showcar-release", "text": "DEVELOP!!! New ShowCar UI release was done. See build here: <https://gitlab.com/web-experience/showcar-ui/pipelines/$CI_BUILD_ID>" }' "$SLACK_HOOK_URL"
+SLACK_HOOK_URL=${SLACK_HOOK_URL%$'\r'}
+curl -X POST --data-urlencode 'payload={"channel": "#as24_web_experience", "username": "showcar-release", "text": "DEVELOP!!! New ShowCar UI release was done. See build here: <https://gitlab.com/web-experience/showcar-ui/pipelines/'"$CI_BUILD_ID"'>" }' "$SLACK_HOOK_URL"
