25c2bbbc759ae4134632011ad9d717081a861c43
Adam <aberesdeak@autoscout24.com>
2016-12-02 15:41:10 +0100

    improvements to slack message

--- deploy/slack.js
+++ deploy/slack.js
@@ -29,7 +29,8 @@ const req = https.request({
 const data = {
     channel: '#as24_web_experience',
     username: 'showcar-release',
-    text: `@channel: DEVELOP!!! New ShowCar UI release was done. See build here: <https://gitlab.com/web-experience/showcar-ui/pipelines/${process.env.CI_BUILD_ID}>`
+    text: `TEST! @channel: DEVELOP!!! New ShowCar UI release was done. See build here: <https://gitlab.com/web-experience/showcar-ui/pipelines/${process.env.CI_BUILD_ID}>`,
+    link_names: 1
 };
 
 req.write(querystring.stringify({payload: JSON.stringify(data)}));
