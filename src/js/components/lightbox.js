09e44e030cda7fbee55e227a96638b18826fd9b6
Diana Zubova <dzubova@autoscout24.com>
2017-01-10 11:17:14 +0100

    added subscription remove support

--- src/js/components/lightbox.js
+++ src/js/components/lightbox.js
@@ -1,5 +1,6 @@
 (function() {
     Array.prototype.forEach.call(document.querySelectorAll('.sc-lightbox-trigger'), (lightboxTrigger) => {
+        let subscription;
         function show() {
             const dataId = lightboxTrigger.getAttribute('data-id');
             const container = document.getElementById(dataId);
@@ -7,38 +8,51 @@
             const overlay = document.createElement('div');
 
             containerClone.className = containerClone.className
-            .replace( /(?:^|\s)sc-hidden(?!\S)/g , ' sc-lightbox sc-grid-col-6' );
+            .replace( /(?:^|\s)sc-hidden(?!\S)/g , ' sc-lightbox' );
             overlay.appendChild(containerClone);
             overlay.setAttribute('class', 'sc-lightbox-overlay sc-grid-row');
 
             const closeElements = overlay.querySelectorAll('.sc-lightbox-close');
             // Events
+            subscription = subscribe(document, removeLightbox(overlay));
             Array.prototype.forEach.call(closeElements, (closeEl) => {
                 closeEl.addEventListener('click', e => {
                     clean(overlay);
+                    subscription.dispose();
                 })
             });
             overlay.addEventListener('click', e => {
                 if (!$(e.target).closest(containerClone).length) {
                     clean(overlay);
+                    subscription.dispose();
                 }
             });
-            document.addEventListener('keydown', removeLightbox);
 
             document.body.appendChild(overlay);
             document.documentElement.setAttribute('class', 'sc-lightbox-scroll');
         }
 
-        function removeLightbox(e) {
-            const overlay = document.querySelector('.sc-lightbox-overlay');
-            if ( e.keyCode == 27 && overlay) {
-                clean(overlay);
+        const removeLightbox = (overlay) => {
+            return function(e) {
+                if ( e.keyCode == 27 ) {
+                    clean(overlay);
+                }
+                if (subscription) {
+                    subscription.dispose();
+                }
+            }
+        }
+
+        const subscribe = (elem, handler) => {
+            elem.addEventListener('keydown', handler);
+            return {
+                dispose: () => elem.removeEventListener('keydown', handler)
             }
         }
 
         const clean = overlay => {
             document.body.removeChild(overlay);
-            document.removeEventListener('keydown', removeLightbox);
+            subscription.dispose();
             document.documentElement.className = '';
         }
 
