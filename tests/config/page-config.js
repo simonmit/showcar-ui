e555b81b550834906cc3cf9c31f04ddc501b8a96
Diana Zubova <dzubova@autoscout24.com>
2017-01-12 11:07:46 +0100

    fixed 200 test

--- tests/config/page-config.js
+++ tests/config/page-config.js
@@ -16,7 +16,9 @@ const countryConfigs = [{
     pages: {
         home: '/',
         list: '/results',
-        detail: '/classified-detail/detail-page/4c990c2d-1f21-3d06-e053-e350040a6fdf'
+        // TODO: find a better solution to refer detail page
+        // detail: '/classified-detail/detail-page/4c990c2d-1f21-3d06-e053-e350040a6fdf'
+        detail: '/offers/gmc-others-f250-apache-longbed-stepside-pick-up-gasoline-beige-40208542-caf6-9737-e053-e350040a6c79'
     }
 }];
 
