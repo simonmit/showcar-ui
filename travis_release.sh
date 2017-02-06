387ecf7215305d9c72e5d7b5323cb202b6f593b1
Adam <aberesdeak@autoscout24.com>
2017-02-06 11:40:29 +0100

    running travis release for master and not for PRs

--- travis_release.sh
+++ travis_release.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-  # only run for master branch, we don't want to release PRs and other branches
-  echo 'we are releasing now...'
-  ls dist -R
-fi
+echo 'we are releasing now...'
+ls dist -R
 
