47b889719f4974375402048c525730cd86538fd8
Adam <aberesdeak@autoscout24.com>
2017-02-06 15:59:23 +0100

    using team email

--- travis_release.sh
+++ travis_release.sh
@@ -4,13 +4,11 @@ set -ev
 
 TARGET_BRANCH=release
 
-echo "$COMMIT_AUTHOR_EMAIL"
-
 mkdir temp-git
 cd temp-git
 git clone "https://${GH_TOKEN}@github.com/AutoScout24/showcar-ui.git" .
 git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
+git config user.email "${GIT_EMAIL}"
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 
 cp -r ../dist .
