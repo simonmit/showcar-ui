8f8eb27bca35439b9ab510abf518cbc5c0b44a8f
Adam <aberesdeak@autoscout24.com>
2017-02-06 14:59:32 +0100

    release script for travis

--- travis_release.sh
+++ travis_release.sh
@@ -5,3 +5,19 @@ set -ev
 echo 'we are releasing now...'
 ls dist -R
 
+mkdir temp-git
+cd temp-git
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+git clone "https://${GH_TOKEN}@github.com/AutoScout24/showcar-ui.git" .
+git checkout release
+
+cp ../dist .
+cp ../src .
+cp ../package.json .
+cp ../History.md .
+
+git add . -A
+git commit -am "Release"
+git push
+
