5cb2234a526dca962692f3f389d340fe96610a1b
Adam <aberesdeak@autoscout24.com>
2017-02-06 16:23:17 +0100

    only cloning release branch

--- travis_release.sh
+++ travis_release.sh
@@ -6,11 +6,11 @@ TARGET_BRANCH=release
 
 mkdir temp-git
 cd temp-git
-git clone "https://${GITHUB_TOKEN}@github.com/AutoScout24/showcar-ui.git" .
+git clone -b $TARGET_BRANCH --single-branch "https://${GITHUB_TOKEN}@github.com/AutoScout24/showcar-ui.git" .
 git config user.name "Travis CI"
 git config user.email "${GIT_EMAIL}"
 git config push.default simple
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+git checkout $TARGET_BRANCH
 
 cp -r ../dist .
 cp -r ../src .
