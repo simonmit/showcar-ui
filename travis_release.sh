4a6a6af17632f9deb9dc862b0f32d50ea802ca01
Adam <aberesdeak@autoscout24.com>
2017-02-06 15:46:21 +0100

    fixed build script

--- travis_release.sh
+++ travis_release.sh
@@ -11,8 +11,8 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 git clone "https://${GH_TOKEN}@github.com/AutoScout24/showcar-ui.git" .
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 
-cp ../dist .
-cp ../src .
+cp -r ../dist .
+cp -r ../src .
 cp ../package.json .
 cp ../History.md .
 
