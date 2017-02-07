417fa56dc372ae2ba82e355f16deb1b5ab0876a3
Adam <aberesdeak@autoscout24.com>
2017-02-07 13:19:45 +0100

    added secure key for travis release

--- travis_release.sh
+++ travis_release.sh
@@ -4,9 +4,14 @@ set -ev
 
 TARGET_BRANCH=release
 
+chmod 600 release_key
+eval `ssh-agent -s`
+ssh-add release_key
+
 mkdir temp-git
 cd temp-git
-git clone -b $TARGET_BRANCH --single-branch "https://${GITHUB_TOKEN}@github.com/AutoScout24/showcar-ui.git" .
+
+git clone -b $TARGET_BRANCH --single-branch "git@github.com:AutoScout24/showcar-ui.git" .
 git config user.name "Travis CI"
 git config user.email "${GIT_EMAIL}"
 git config push.default simple
