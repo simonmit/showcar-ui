80590305cf4573c92a349caafd0a8bc3d61c9931
Andrei Solovev <andrei.solovev@scout24.com>
2017-02-16 11:04:21 +0100

    update travis for gh-pages

--- travis_release.sh
+++ travis_release.sh
@@ -2,20 +2,21 @@
 
 set -ev
 
-TARGET_BRANCH=release
+RELEASE_BRANCH=release
+DOCS_BRANCH=gh-pages
 
 chmod 600 release_key
 eval `ssh-agent -s`
 ssh-add release_key
 
-mkdir temp-git
-cd temp-git
+mkdir temp-release
+cd temp-release
 
-git clone -b $TARGET_BRANCH --single-branch "git@github.com:AutoScout24/showcar-ui.git" .
+git clone -b $RELEASE_BRANCH --single-branch "git@github.com:AutoScout24/showcar-ui.git" .
 git config user.name "Travis CI"
 git config user.email "${GIT_EMAIL}"
 git config push.default simple
-git checkout $TARGET_BRANCH
+git checkout $RELEASE_BRANCH
 
 cp -r ../dist .
 cp -r ../src .
@@ -24,5 +25,26 @@ cp ../History.md .
 
 git add . -A
 git commit -am "Release"
-git push origin $TARGET_BRANCH
+git push origin $RELEASE_BRANCH
 
+
+###### DOCS ######
+cd ..
+mkdir temp-gh-pages
+cd  temp-gh-pages
+
+git clone -b $DOCS_BRANCH --single-branch "git@github.com:AutoScout24/showcar-ui.git" .
+git config user.name "Travis CI"
+git config user.email "${GIT_EMAIL}"
+git config push.default simple
+git checkout $DOCS_BRANCH
+
+cp -r ../docs .
+cp -r ../dist docs/lib
+cp -r ../06-components docs/components
+cp  ../package.json .
+cp ../History.md .
+
+git add . -A
+git commit -am "Release"
+git push origin $DOCS_BRANCH
