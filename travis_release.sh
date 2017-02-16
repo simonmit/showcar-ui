d3b355d12d4bfc2fa19aaac584f2114f53903a6a
Andrei Solovev <andrei.solovev@scout24.com>
2017-02-16 13:45:01 +0100

    remove files after clone

--- travis_release.sh
+++ travis_release.sh
@@ -35,15 +35,14 @@ cd ..
 mkdir temp-gh-pages
 cd  temp-gh-pages
 
-#remove all files except .gitignore and all inside.git "shopt -s extglob" extends bash
-shopt -s extglob
-rm -rf !(.git*)
-
 git clone -b $DOCS_BRANCH --single-branch "git@github.com:AutoScout24/showcar-ui.git" .
 git config user.name "Travis CI"
 git config user.email "${GIT_EMAIL}"
 git config push.default simple
 
+#remove all files except .gitignore and all inside.git "shopt -s extglob" extends bash
+shopt -s extglob
+rm -rf !(.git*)
 
 cp -r ../docs .
 cp -r ../dist docs/lib
