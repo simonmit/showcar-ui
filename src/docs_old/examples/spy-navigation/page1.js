4ee4ac9b166a68f46fda48007dc2e853ef9647ee
Andrei Solovev <andrei.solovev@scout24.com>
2017-02-16 11:33:58 +0100

    remove everything

--- src/docs_old/examples/spy-navigation/page1.js
+++
@@ -1,7 +0,0 @@
-window.showcar.spyNavigation({
-    stickPosFn: function(scrollTop, stickToElem, componentElem) {
-        var info = componentElem.querySelector('.page-navigation-title');
-        return scrollTop > stickToElem.offsetTop - info.getBoundingClientRect().height;
-    },
-    unstickPosFn: null
-});
