e5715f391904616cc632263223b1ca33683bffd1
Andrei Solovev <andrei.solovev@scout24.com>
2017-03-06 15:40:39 +0100

    clean up

--- config/webdriver.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    local: {
-        configFile: './tests/config/wdio.local.conf.js'
-    },
-    cbt: {
-        configFile: './tests/config/wdio.crossbrowsertesting.conf.js'
-    }
-};
