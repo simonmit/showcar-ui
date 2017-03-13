8971ffe5ccfe88d7fc188e7f54d40c708b33ed87
meckert <mathias.eckert@autoscout24.com>
2017-03-13 21:07:59 +0100

    added additional stylelint rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,6 +4,13 @@ module.exports = {
         "indentation": 2,
         "length-zero-no-unit": true,
         "no-duplicate-selectors": true,
-        "string-quotes": "single"
+        "string-quotes": "single",
+        "value-no-vendor-prefix": true,
+        "property-no-vendor-prefix": true,
+        "declaration-block-no-duplicate-properties": true,
+        "block-no-empty": true,
+        "selector-no-id": true,
+        "rule-empty-line-before": "always-multi-line",
+        "no-extra-semicolons": true
     }
 };
