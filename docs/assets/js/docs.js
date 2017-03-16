197353c08de91e85aa1bbfb9c46343c64b15cba1
Travis CI <#AS24-Web-Experience-Team-ds@scout24.com>
2017-03-16 11:56:06 +0000

    Release

--- docs/assets/js/docs.js
+++ docs/assets/js/docs.js
@@ -109,18 +109,3 @@ window.addEventListener('load', function () {
  }
  });
 */
-
-
-/*setTimeout(()=>{ // temporary hack
- (function ($) {
- var paginationElement = document.querySelector('.sc-pagination'),
- itemsPerPage = 20,
- activePage = 1,
- totalCount = 800,
- urlTemplate = 'http://www.autoscout24.com/listWithPagination?page={page}&size={size}';
- 
- if (paginationElement) {
- new Pager(paginationElement, itemsPerPage, activePage, totalCount, urlTemplate);
- }
- })(window.Zepto);
- },7000)*/
