e8b73d545cb751ef078634a344c6dd406bfe6db9
Travis CI <#AS24-Web-Experience-Team-ds@scout24.com>
2017-03-16 10:10:48 +0000

    Release

--- tasks/generateHtml.js
+++ tasks/generateHtml.js
@@ -1,10 +1,5 @@
 module.exports = () => {
     const fs = require('fs');
     const html = require('../helpers/renderHtml.js')();
-    fs.writeFileSync("./docs/index.html", html, err => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("HTML file was saved!");
-    });
+    fs.writeFileSync("./docs/index.html", html)
 }
--- tasks/generateJson.js
+++ tasks/generateJson.js
@@ -1,10 +1,5 @@
 module.exports = () => {
     const fs = require('fs');
     const globalJSON = require('../helpers/renderContentJson.js')();
-    fs.writeFileSync("./docs/globalJSON.json", JSON.stringify(globalJSON, null, 4), err => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("JSON file was saved!");
-    });
+    fs.writeFileSync("./docs/globalJSON.json", JSON.stringify(globalJSON, null, 4))
 }
