a0b37ff63d9c2fbdad96c48cb41e2476435b1cd6
Andrei Solovev <andrei.solovev@scout24.com>
2017-03-16 12:56:16 +0100

    fix to DOMContentLoaded

--- src/06-components/atoms/rotating-arrow/rotating-arrow.js
+++ src/06-components/atoms/rotating-arrow/rotating-arrow.js
@@ -1,10 +1,10 @@
 export default () => {
-   window.onload = ()=> {
+    window.addEventListener('DOMContentLoaded', function() {
        Array.prototype.forEach.call(document.querySelectorAll('[data-toggle="arrow"]'), (arrow) => {
            let arrowEl = $(arrow);
            $(arrowEl.data('target')).on('click', () => {
                arrowEl.toggleClass('open')
            });
        });
-   }
+    }, true);
 };
