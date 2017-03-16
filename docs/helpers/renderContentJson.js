862d80c56eb30f3352d22bc7a6cfa4e9ee8c7c0a
Andrei Solovev <andrei.solovev@scout24.com>
2017-03-16 11:56:28 +0100

    small clean up

--- docs/helpers/renderContentJson.js
+++ docs/helpers/renderContentJson.js
@@ -35,44 +35,10 @@ const getFiles = (route, name) => {
     return routes;
 }
 
-const getGroup = (route) => {
-    return route.split('/').slice(- 2, - 1)[0];
-}
-
-const setAttr = (paths) => {
-    const srcDir = path.parse(paths).dir;
-    const dirsArr = srcDir.split('/').slice(- 3, - 1);
-    const name = path.parse(paths).name;
-    const markDown = marked(fs.readFileSync(paths, 'utf8'));
-    
-    const element = {
-        name,
-        type: dirsArr[0],
-        group: dirsArr[1],
-        srcDir,
-        markDown: JSON.stringify(markDown),
-    }
-    
-    const htmlPaths = paths.split('.md')[0] + '.html';
-    if (fs.existsSync(htmlPaths)) {
-        const html = fs.readFileSync(htmlPaths, 'utf8');
-        element.html = JSON.stringify(html);
-        element.codeExample = JSON.stringify(entities.encode(html));
-    }
-    
-    const jsPaths = paths.split('.md')[0] + '.js';
-    if (fs.existsSync(jsPaths)) {
-        element.jsExample = JSON.stringify(fs.readFileSync(jsPaths, 'utf8'));
-    }
-    
-    return element;
-}
-
 const setObj = (type, route, name) => {
     let files = getFiles(route, name);
     return files
         .map((file) => {
-            // console.log(type)
             const group = file.group;
             const paths = file.mdPath;
             const srcDir = path.parse(paths).dir;
