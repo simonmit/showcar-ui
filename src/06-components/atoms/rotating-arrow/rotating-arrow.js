c116b1c11c6d756528e4a4f4cf973f363a327e5c
Travis CI <#AS24-Web-Experience-Team-ds@scout24.com>
2017-03-16 11:38:41 +0000

    Release

--- src/06-components/atoms/rotating-arrow/rotating-arrow.js
+++ src/06-components/atoms/rotating-arrow/rotating-arrow.js
@@ -1,6 +1,10 @@
 export default () => {
-    Array.prototype.forEach.call(document.querySelectorAll('[data-toggle="arrow"]'), (arrow) => {
-        let arrowEl = $(arrow);
-        $(arrowEl.data('target')).on('click', () => arrowEl.toggleClass('open'));
-    });
+   window.onload = ()=> {
+       Array.prototype.forEach.call(document.querySelectorAll('[data-toggle="arrow"]'), (arrow) => {
+           let arrowEl = $(arrow);
+           $(arrowEl.data('target')).on('click', () => {
+               arrowEl.toggleClass('open')
+           });
+       });
+   }
 };
