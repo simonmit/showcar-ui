2d968d466c09fff3552fa0af352823c949a24681
meckert <mathias.eckert@autoscout24.com>
2017-03-27 17:28:59 +0200

    added additional information in docs about spy-navigation and more

--- src/06-components/organisms/navigation/docs/navigation.md
+++ src/06-components/organisms/navigation/docs/navigation.md
@@ -1,4 +1,7 @@
 <h2>Navigation <span class="status refactor">Needs refactoring</span></h2>
+
+The navigation currently only works when used in combination with contentservice fragment.
+
 <style>
 #navigation .sample{
 background-color: #f4f4f4;
--- src/06-components/organisms/spy-navigation/docs/spy-navigation.js
+++ src/06-components/organisms/spy-navigation/docs/spy-navigation.js
@@ -1,21 +1,23 @@
 // This component have to be initialized with an object with two functions:
 
-showcar.spyNavigation({
-    stickPosFn: function(scrollTop, stickToElem, componentElem) {
-        // scrollTop - amount of window scroll
-        // stickToElem - in the example, #data-stick-when
-        // componentElem - in the example, .sc-spy-navigation
-        // Do your calculations, return Boolean
-    },
-    unstickPosFn: function(scrollTop, stickToElem, componentElem) {
-        // scrollTop - amount of window scroll
-        // stickToElem - in the example, #data-stick-when
-        // componentElem - in the example, .sc-spy-navigation
-        // Do your calculations, return Boolean
-    }
-});
-
 // Each function takes 3 arguments:
 // scrollTop - scroll amount
 // stickToElem - the element to which the navigation should stick to
 // componentElem - the component element (the one with the root class ".sc-spy-navigation")
+
+showcar.spyNavigation({
+    stickPosFn: function(scrollTop, stickToElem, componentElem) {
+        // Define when the navigation should be sticky, return Boolean
+    },
+    unstickPosFn: function(scrollTop, stickToElem, componentElem) {
+        // Define when the navigation should not be sticky, return Boolean
+    }
+});
+
+// Example
+const stickyYStartPosition = $('.sc-spy-navigation').offset().top;
+
+showcar.spyNavigation({
+    stickPosFn: (scrollTop, stickToElem, componentElem) => scrollTop > stickyYStartPosition,
+    unstickPosFn: (scrollTop, stickToElem, componentElem) => scrollTop <= stickyYStartPosition
+});
