7385cb766ab0f5f92db11bf4a4ebfd489c7626d2
Diana Zubova <dzubova@autoscout24.com>
2017-04-04 16:56:23 +0200

    added input tests

---
+++ src/06-components/atoms/input/specs/input.layout.js
@@ -0,0 +1,82 @@
+module.exports = (frame, assert) => {
+    describe('Checkbox', function () {
+        it('default input is hidden', function () {
+            var checkboxInput = frame.get('#checkbox #as24-checkbox-1');
+            assert.equal(checkboxInput.getRawStyle('opacity'), 0, 'opacity should be 0');
+        });
+
+        it('label is rendered', function () {
+            var checkboxLabel = frame.get('#checkbox label[for=as24-checkbox-1]');
+
+            checkboxLabel.assert({
+                rendered: true
+            });
+        });
+
+        it('label has proper height', function () {
+            var checkboxLabel = frame.get('#checkbox label[for=as24-checkbox-1]');
+
+            checkboxLabel.assert({
+                height: 24
+            });
+        });
+    });
+    describe('Input', function () {
+        it('has a proper height', function () {
+            var input = frame.get('#input-default input[type=text].sc-input');
+
+            input.assert({
+                height: 40
+            });
+        });
+
+        it('is fullwidth', function () {
+            var container = frame.get('#separate-content');
+            var input = frame.get('#input-default input[type=text].sc-input');
+
+            input.assert({
+                width: container.width
+            });
+        });
+    });
+    describe('Radio button', function () {
+        it('default input is hidden', function () {
+            var radioInput = frame.get('#radio-button #as24-radio-1');
+            assert.equal(radioInput.getRawStyle('opacity'), 0, 'opacity should be 0');
+        });
+
+        it('label is rendered', function () {
+            var radioLabel = frame.get('#radio-button label[for=as24-radio-1]');
+
+            radioLabel.assert({
+                rendered: true
+            });
+        });
+
+        it('label has proper height', function () {
+            var radioLabel = frame.get('#radio-button label[for=as24-radio-1]');
+
+            radioLabel.assert({
+                height: 24
+            });
+        });
+    });
+    describe('Textarea', function () {
+        it('is rendered', function () {
+            var textarea = frame.get('#textarea');
+
+            textarea.assert({
+                rendered: true
+            });
+        });
+
+        it('is fullwidth', function () {
+            var container = frame.get('#separate-content');
+            var textarea = frame.get('#textarea textarea.sc-input');
+
+            textarea.assert({
+                width: container.width
+            });
+        });
+    });
+};
