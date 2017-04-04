6dc1f6bb1b4646abf87872eb53f799499a532f6f
Andrei Solovev <andrei.solovev@scout24.com>
2017-04-04 16:34:58 +0200

    now require works with glob

--- testQuixote.js
+++ testQuixote.js
@@ -1,21 +1,22 @@
-window.__karma__.loaded = function () {}; //  prevent of execution mocha
+window.__karma__.loaded = function () {}; //  prevent of execution mocha  https://zerokspot.com/weblog/2013/07/12/delay-test-execution-in-karma/
 var quixote = require('quixote');
 var assert = require('chai').assert;
 var frame;
 setTimeout(function () {
-    window.__karma__.start(); //execute mocha; Wait while frame will load
+    window.__karma__.start(); //execute mocha; Wait while frame will load https://github.com/karma-runner/karma/blob/v0.8.6/adapter/mocha.wrapper#L6
 }, 2000);
 
 var runTest = function (browserWidth) {
     frame = quixote.createFrame({
         src: 'http://localhost:9876/docs/atoms/',     // karma url with port //change to http://localhost:9876
         width: browserWidth
-    },function () { //keep callback
+    }, function () { //keep callback
 
     });
     describe('Device width: ' + browserWidth, function () {
-        require('./src/06-components/atoms/button/specs/button.layout.js')(frame, assert);
-        require('./src/06-components/atoms/custom-dropdown/specs/custom-dropdown.layout.js')(frame, assert);
+        require('./src/06-components/**/specs/*layout.js', { mode: 'list' }).forEach(function (file) {
+            file.module(frame, assert);
+        });
     });
     beforeEach(function () {
         frame.reset();
