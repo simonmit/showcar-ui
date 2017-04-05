07104a4775ee395109429b6197824d1b14c1343a
meckert <mathias.eckert@autoscout24.com>
2017-04-05 18:19:09 +0200

    removed backstopJS code

--- tests/config/wdio.local.conf.js
+++
@@ -1,6 +0,0 @@
-exports.config = Object.assign({}, require('./wdio.base.conf').config, {
-    capabilities: [{
-        browserName: 'chrome'
-    }],
-    baseUrl: 'http://127.0.0.1:63342/showcar-ui/public'
-});
