181b7d89907e3181ca6439b7c87422cd97c418cd
Diana Zubova <dzubova@autoscout24.com>
2017-04-06 11:23:58 +0200

    updated molecules tests

--- src/06-components/molecules/input-group/specs/input-group.layout.js
+++ src/06-components/molecules/input-group/specs/input-group.layout.js
@@ -12,6 +12,7 @@ module.exports = frame => {
                 width: container.width
             });
         });
+
         it('have proper height', function () {
             var inputGroup = frame.get('#input-group .sc-input-group');
             var inputGroupRadio = frame.get('#input-group-radio .sc-input-group-radio');
@@ -21,6 +22,32 @@ module.exports = frame => {
             });
             inputGroupRadio.assert({
                 height: 40
+            });
+        });
+
+        it('default elements are positioned next to each other', function () {
+            var firstEl = frame.get('#input-group .sc-input-group input:first-of-type');
+            var secondEl = frame.get('#input-group .sc-input-group input:nth-of-type(2)');
+            var thirdEl = frame.get('#input-group .sc-input-group .sc-btn-bob');
+
+            firstEl.assert({
+                right: secondEl.left.plus(1)
+            });
+            secondEl.assert({
+                right: thirdEl.left.plus(1)
+            });
+        });
+
+        it('radio elements are positioned next to each other', function () {
+            var firstEl = frame.get('#input-group-radio .sc-input-group-radio label:first-of-type');
+            var secondEl = frame.get('#input-group-radio .sc-input-group-radio label:nth-of-type(2)');
+            var thirdEl = frame.get('#input-group-radio .sc-input-group-radio label:nth-of-type(3)');
+
+            firstEl.assert({
+                right: secondEl.left
+            });
+            secondEl.assert({
+                right: thirdEl.left
             });
         });
     });
