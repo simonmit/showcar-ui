776064c6c35c61bb3e57974ce35f5bccafd06f47
Andrei Solovev <andrei.solovev@scout24.com>
2017-05-02 10:42:28 +0200

    add collision detection

--- src/06-components/atoms/tooltip/specs/tooltip.specs.js
+++ src/06-components/atoms/tooltip/specs/tooltip.specs.js
@@ -21,18 +21,52 @@ module.exports = function (frame, assert, browserWidth, helper) {
             tooltipContent = frame.get('.sc-tooltip-content.sc-tooltip-shown').toDomElement();
             assert.include(tooltipContent.innerText, 'Information in tooltip', 'contains');
         });
-        /*
-         it('Check position of tooltip', function (done) {
-         setTimeout(function () {
-         helper.hoverOn(tooltip);
-         setTimeout(function () {
-         tooltipContent = frame.get('#tooltip .sc-tooltip-shown').toDomElement();
-         assert.isOk(helper.hasClass(tooltipContent,'sc-tooltip-top'), 'position top');
-         done();
-         }, 100); //wait for text
-         }, 100); //wait for reseting
-         });
-         */
+
+        it('Check normal position of tooltip', function () {
+            helper.hoverOn(tooltip);
+            tooltipContent = frame.get('.sc-tooltip-content.sc-tooltip-shown').toDomElement();
+            assert.isOk(helper.hasClass(tooltipContent, 'sc-tooltip-top'), 'position top');
+        });
+
+        it('Check top-left position of tooltip', function () {
+            tooltip.style.position = 'fixed';
+            tooltip.style.left = 0;
+            tooltip.style.top = 0;
+            helper.hoverOn(tooltip);
+            tooltipContent = frame.get('.sc-tooltip-content.sc-tooltip-shown').toDomElement();
+            assert.isOk(helper.hasClass(tooltipContent, 'sc-tooltip-bottom'), 'position top');
+            assert.isOk(helper.hasClass(tooltipContent, 'sc-tooltip-right'), 'position left');
+        });
+
+        it('Check top-right position of tooltip', function () {
+            tooltip.style.position = 'fixed';
+            tooltip.style.right = 0;
+            tooltip.style.top = 0;
+            helper.hoverOn(tooltip);
+            tooltipContent = frame.get('.sc-tooltip-content.sc-tooltip-shown').toDomElement();
+            assert.isOk(helper.hasClass(tooltipContent, 'sc-tooltip-bottom'), 'position top');
+            assert.isOk(helper.hasClass(tooltipContent, 'sc-tooltip-left'), 'position right');
+        });
+
+        it('Check bottom-left position of tooltip', function () {
+            tooltip.style.position = 'fixed';
+            tooltip.style.left = 0;
+            tooltip.style.bottom = 0;
+            helper.hoverOn(tooltip);
+            tooltipContent = frame.get('.sc-tooltip-content.sc-tooltip-shown').toDomElement();
+            assert.isOk(helper.hasClass(tooltipContent, 'sc-tooltip-top'), 'position bottom');
+            assert.isOk(helper.hasClass(tooltipContent, 'sc-tooltip-right'), 'position left');
+        });
+
+        it('Check bottom-right position of tooltip', function () {
+            tooltip.style.position = 'fixed';
+            tooltip.style.right = 0;
+            tooltip.style.bottom = 0;
+            helper.hoverOn(tooltip);
+            tooltipContent = frame.get('.sc-tooltip-content.sc-tooltip-shown').toDomElement();
+            assert.isOk(helper.hasClass(tooltipContent, 'sc-tooltip-top'), 'position bottom');
+            assert.isOk(helper.hasClass(tooltipContent, 'sc-tooltip-left'), 'position right');
+        });
 
     });
 };
