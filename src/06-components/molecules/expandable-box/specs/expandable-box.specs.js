6f45234cbbb090e765a0d737b8b37f1109cfefcf
Diana Zubova <dzubova@autoscout24.com>
2017-05-08 17:16:02 +0200

    Added expandable box tests

--- src/06-components/molecules/expandable-box/specs/expandable-box.specs.js
+++ src/06-components/molecules/expandable-box/specs/expandable-box.specs.js
@@ -22,40 +22,4 @@ module.exports = function (frame, assert, browserWidth, helper) {
             })
         });
     });
-    describe('Expandable box adaptive', function () {
-        var box;
-        var content;
-
-        beforeEach(function () {
-            box = frame.get('#expandable-box-adaptive .sc-expandable-box__label').toDomElement();
-            content = frame.get('#expandable-box-adaptive .sc-expandable-box__content');
-        })
-
-        afterEach(function (done) {
-            helper.reload(frame, done)
-        })
-
-        if (browserWidth > 767) {
-            it('is open on desktop', function () {
-                content.assert({
-                    rendered: true
-                })
-                helper.click(box);
-                content.assert({
-                    rendered: true
-
-                })
-            });
-        } else {
-            it('opens on click on mobile', function () {
-                content.assert({
-                    rendered: false
-                })
-                helper.click(box);
-                content.assert({
-                    rendered: true
-                })
-            });
-        }
-    });
 };
