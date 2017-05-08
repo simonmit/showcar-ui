890a3080c35c8b03860ac66820de6e31efbde9db
Diana Zubova <dzubova@autoscout24.com>
2017-05-08 14:53:17 +0200

    added expandable box tests

---
+++ src/06-components/molecules/expandable-box/specs/expandable-box.specs.js
@@ -0,0 +1,61 @@
+module.exports = function (frame, assert, browserWidth, helper) {
+    describe('Expandable box', function () {
+        var box;
+        var content;
+
+        beforeEach(function () {
+            box = frame.get('#expandable-box .sc-expandable-box__label').toDomElement();
+            content = frame.get('#expandable-box .sc-expandable-box__content');
+        })
+
+        afterEach(function (done) {
+            helper.reload(frame, done)
+        })
+
+        it('opens on click', function () {
+            content.assert({
+                rendered: false
+            })
+            helper.click(box);
+            content.assert({
+                rendered: true
+            })
+        });
+    });
+    describe('Expandable box adaptive', function () {
+        var box;
+        var content;
+
+        beforeEach(function () {
+            box = frame.get('#expandable-box-adaptive .sc-expandable-box__label').toDomElement();
+            content = frame.get('#expandable-box-adaptive .sc-expandable-box__content');
+        })
+
+        afterEach(function (done) {
+            helper.reload(frame, done)
+        })
+
+        if (browserWidth > 767) {
+            it('is open on desktop', function () {
+                content.assert({
+                    rendered: true
+                })
+                helper.click(box);
+                content.assert({
+                    rendered: true
+
+                })
+            });
+        } else {
+            it('opens on click on mobile', function () {
+                content.assert({
+                    rendered: false
+                })
+                helper.click(box);
+                content.assert({
+                    rendered: true
+                })
+            });
+        }
+    });
+};
