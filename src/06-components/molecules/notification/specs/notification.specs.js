95e3c6cd93f0e4d200ba30f0d7cdda7f5779a927
meckert <mathias.eckert@autoscout24.com>
2017-05-11 16:51:19 +0200

    added new data-trigger attribute to notifications in order to make docs / tests work properly

--- src/06-components/molecules/notification/specs/notification.specs.js
+++ src/06-components/molecules/notification/specs/notification.specs.js
@@ -7,56 +7,56 @@ module.exports = function (frame, assert, browserWidth, helper) {
         beforeEach(function () {
             container = frame.get('#notification-closable #example-notification-target');
             trigger = frame.get('#notification-closable [data-id=succ2]').toDomElement();
-        })
+        });
 
         afterEach(function (done) {
             helper.reload(frame, done)
         })
 
-        // it('shows on click', function (done) {
-        //     helper.click(trigger);
-        //     var notification = frame.get('as24-notification#succ2');
-        //     setTimeout(function () {
-        //         notification.assert({
-        //             rendered: true,
-        //             width: container.width,
-        //             top: container.bottom
-        //         });
-        //         done();
-        //     }, 1000); //waiting for animation
-        // });
-        //
-        // it('hides if cross is clicked', function (done) {
-        //     var notification = frame.get('as24-notification#succ2');
-        //     var cross = frame.get('as24-notification#succ2 a').toDomElement();
-        //     helper.click(trigger);
-        //     helper.click(cross);
-        //     setTimeout(function () {
-        //         notification.assert({
-        //             height: 0
-        //         });
-        //         done();
-        //     }, 1000); //waiting for animation
-        // });
-        //
-        // it('two notifications sticks on each other', function (done) {
-        //     var triggerInfo = frame.get('#notification-closable [data-id=info2]').toDomElement();
-        //     var cross = frame.get('as24-notification#succ2 a').toDomElement();
-        //     helper.click(trigger);
-        //     helper.click(triggerInfo);
-        //     var notification = frame.get('as24-notification#succ2');
-        //     var notificationInfo = frame.get('as24-notification#info2');
-        //     notificationInfo.assert({
-        //         top: notification.bottom
-        //     });
-        //     helper.click(cross);
-        //     setTimeout(function () {
-        //         notificationInfo.assert({
-        //             top: container.bottom
-        //         });
-        //         done();
-        //     }, 1000); //waiting for animation
-        // });
+        it('shows on click', function (done) {
+            helper.click(trigger);
+            var notification = frame.get('as24-notification#succ2');
+            setTimeout(function () {
+                notification.assert({
+                    rendered: true,
+                    width: container.width,
+                    top: container.bottom
+                });
+                done();
+            }, 1000); //waiting for animation
+        });
+
+        it('hides if cross is clicked', function (done) {
+            var notification = frame.get('as24-notification#succ2');
+            var cross = frame.get('as24-notification#succ2 a').toDomElement();
+            helper.click(trigger);
+            helper.click(cross);
+            setTimeout(function () {
+                notification.assert({
+                    height: 0
+                });
+                done();
+            }, 1000); //waiting for animation
+        });
+
+        it('two notifications sticks on each other', function (done) {
+            var triggerInfo = frame.get('#notification-closable [data-id=info2]').toDomElement();
+            var cross = frame.get('as24-notification#succ2 a').toDomElement();
+            helper.click(trigger);
+            helper.click(triggerInfo);
+            var notification = frame.get('as24-notification#succ2');
+            var notificationInfo = frame.get('as24-notification#info2');
+            notificationInfo.assert({
+                top: notification.bottom
+            });
+            helper.click(cross);
+            setTimeout(function () {
+                notificationInfo.assert({
+                    top: container.bottom
+                });
+                done();
+            }, 1000); //waiting for animation
+        });
 
         // it('notification is sticky', function (done) {
         //     // var scrollPoint = frame.get('#notification-closable').toDomElement();
