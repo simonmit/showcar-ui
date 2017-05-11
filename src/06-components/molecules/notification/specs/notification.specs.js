be51cd47888594bedd2462f1b7214b5fef8454eb
Diana Zubova <dzubova@autoscout24.com>
2017-05-11 11:21:48 +0200

    tests are green locally

--- src/06-components/molecules/notification/specs/notification.specs.js
+++ src/06-components/molecules/notification/specs/notification.specs.js
@@ -13,68 +13,64 @@ module.exports = function (frame, assert, browserWidth, helper) {
             helper.reload(frame, done)
         })
 
-        // it('shows on click', function (done) {
-        //     helper.click(trigger);
-        //     var notification = frame.get('as24-notification#succ2');
-        //     // notification.assert({
-        //     //     rendered: true,
-        //     //     width: container.width,
-        //     //     top: container.bottom
-        //     // });
+        it('shows on click', function (done) {
+            helper.click(trigger);
+            var notification = frame.get('as24-notification#succ2');
+            setTimeout(function () {
+                notification.assert({
+                    rendered: true,
+                    width: container.width,
+                    top: container.bottom
+                });
+                done();
+            }, 1000); //waiting for animation
+        });
 
-        //     setTimeout(function () {
-        //         notification.assert({
-        //             rendered: true,
-        //             width: container.width,
-        //             top: container.bottom
-        //         });
-        //         done();
-        //     }, 3000); //waiting for animation
-        // });
+        it('hides if cross is clicked', function (done) {
+            var notification = frame.get('as24-notification#succ2');
+            var cross = frame.get('as24-notification#succ2 a').toDomElement();
+            helper.click(trigger);
+            helper.click(cross);
+            setTimeout(function () {
+                notification.assert({
+                    height: 0
+                });
+                done();
+            }, 1000); //waiting for animation
+        });
 
-        // it('hides if cross is clicked', function (done) {
-        //     var notification = frame.get('as24-notification#succ2');
-        //     var cross = frame.get('as24-notification#succ2 a').toDomElement();
-        //     helper.click(trigger);
-        //     helper.click(cross);
-        //     setTimeout(function () {
-        //         notification.assert({
-        //             height: 0
-        //         });
-        //         done();
-        //     }, 1500); //waiting for animation
-        // });
+        it('two notifications sticks on each other', function (done) {
+            var triggerInfo = frame.get('#notification-closable [data-id=info2]').toDomElement();
+            var cross = frame.get('as24-notification#succ2 a').toDomElement();
+            helper.click(trigger);
+            helper.click(triggerInfo);
+            var notification = frame.get('as24-notification#succ2');
+            var notificationInfo = frame.get('as24-notification#info2');
+            notificationInfo.assert({
+                top: notification.bottom
+            });
+            helper.click(cross);
+            setTimeout(function () {
+                notificationInfo.assert({
+                    top: container.bottom
+                });
+                done();
+            }, 1000); //waiting for animation
+        });
 
-        // it('two notifications sticks on each other', function (done) {
-        //     var triggerInfo = frame.get('#notification-closable [data-id=info2]').toDomElement();
-        //     var cross = frame.get('as24-notification#succ2 a').toDomElement();
-        //     helper.click(trigger);
-        //     helper.click(triggerInfo);
-        //     var notification = frame.get('as24-notification#succ2');
-        //     var notificationInfo = frame.get('as24-notification#info2');
-        //     notificationInfo.assert({
-        //         top: notification.bottom
-        //     });
-        //     helper.click(cross);
-        //     setTimeout(function () {
-        //         notificationInfo.assert({
-        //             top: container.bottom
-        //         });
-        //         done();
-        //     }, 3000); //waiting for animation
-        // });
-
-        // it('notification is sticky', function (done) {
-        //     helper.click(trigger);
-        //     var header = frame.get('#small-footer').toDomElement();
-        //     var notification = frame.get('.sc-notification-container');
-        //     header.scrollIntoView(true);
-        //     setTimeout(function () {
-        //         notification.assert({
-        //             top: frame.viewport().top
-        //         });
-        //         done();
-        //     }, 3000); //waiting for scrolling
-        // });
+        it('notification is sticky', function (done) {
+            // var scrollPoint = frame.get('#notification-closable').toDomElement();
+            // scrollPoint.scrollIntoView(true);
+            helper.click(trigger);
+            var header = frame.get('#small-footer').toDomElement();
+            header.scrollIntoView(true);
+            setTimeout(function () {
+                var notification = frame.get('.sc-notification-container');
+                notification.assert({
+                    top: frame.viewport().top
+                });
+                done();
+            }, 2000); //waiting for scrolling
+        });
     });
 };
