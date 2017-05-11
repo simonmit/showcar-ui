959e469c42a88ccc3ab51b386155a2a1d43a2688
Travis CI <#AS24-Web-Experience-Team-ds@scout24.com>
2017-05-11 07:33:59 +0000

    Release

--- src/06-components/organisms/card-group/specs/card-group.specs.js
+++ src/06-components/organisms/card-group/specs/card-group.specs.js
@@ -1,31 +1,42 @@
-module.exports = function (frame, assert, browserWidth) {
-    describe('Card group', function () {
-        it('car element text is centered', function () {
-            var cards = frame.getAll('#card-group .sc-card__image');
-            var textModel = frame.getAll('#card-group .sc-card__label div:first-child');
-            var textOffer = frame.getAll('#card-group .sc-card__label div:nth-child(2)');
-
-            assert(cards.length() > 0, 'we have no cards on page');
-            assert(textModel.length() > 0, 'we have no model texts on page');
-            assert(textOffer.length() > 0, 'we have no offer texts on page');
-            for (var i = 0; i < cards.length() - 1; i++) {
-                textModel.at(i).assert({
-                    center: cards.at(i).center,
-                    top: cards.at(i).bottom.plus(16),
-                });
-                textOffer.at(i).assert({
-                    center: cards.at(i).center,
-                    top: textModel.at(i).bottom,
-                });
-            }
-        });
+module.exports = function (frame, assert, browserWidth, helper) {
+    describe('Card group {LAYOUT}', function () {
         if (browserWidth < 640) {
-            it('cars shown in two rows', function () {
+            it('car element text is centered', function () {
+                var cards = frame.getAll('#card-group .sc-card__image');
+                var textModel = frame.getAll('#card-group .sc-card__label div:first-child');
+                var textOffer = frame.getAll('#card-group .sc-card__label div:nth-child(2)');
+
+                assert(cards.length() > 0, 'we have no cards on page');
+                assert(textModel.length() > 0, 'we have no model texts on page');
+                assert(textOffer.length() > 0, 'we have no offer texts on page');
+                for (var i = 0; i < 3; i++) { // we only have 4 shown
+                    textModel.at(i).assert({
+                        center: cards.at(i).center,
+                        top: cards.at(i).bottom.plus(16),
+                    });
+                    textOffer.at(i).assert({
+                        center: cards.at(i).center,
+                        top: textModel.at(i).bottom,
+                    });
+                }
+            });
+
+            it('more link is shown', function () {
+                var cardList = frame.get('#card-group .sc-cards__list');
+                var link = frame.get('#card-group .sc-cards__show');
+                link.assert({
+                    rendered: true,
+                    top: cardList.bottom.plus(28)
+                })
+            });
+
+            it('only 4 first cars are shown', function () {
                 var cardList = frame.get('#card-group .sc-cards__list');
                 var firstCard = frame.get('#card-group .sc-card:first-of-type');
                 var secondCard = frame.get('#card-group .sc-card:nth-child(2)');
                 var thirdCard = frame.get('#card-group .sc-card:nth-child(3)');
                 var forthCard = frame.get('#card-group .sc-card:nth-child(4)');
+                var fifthCard = frame.get('#card-group .sc-card:nth-child(5)');
 
                 firstCard.assert({
                     top: cardList.top,
@@ -45,56 +56,164 @@ module.exports = function (frame, assert, browserWidth) {
                     right: cardList.right,
                     left: thirdCard.right
                 });
+                fifthCard.assert({
+                    rendered: false
+                });
             });
         } else if (640 <= browserWidth && browserWidth < 720) {
-            it('3 cars are shown on top, 1 is on the bottom', function () {
+            it('car element text is centered', function () {
+                var cards = frame.getAll('#card-group .sc-card__image');
+                var textModel = frame.getAll('#card-group .sc-card__label div:first-child');
+                var textOffer = frame.getAll('#card-group .sc-card__label div:nth-child(2)');
+
+                assert(cards.length() > 0, 'we have no cards on page');
+                assert(textModel.length() > 0, 'we have no model texts on page');
+                assert(textOffer.length() > 0, 'we have no offer texts on page');
+                for (var i = 0; i < cards.length() - 1; i++) {
+                    textModel.at(i).assert({
+                        center: cards.at(i).center,
+                        top: cards.at(i).bottom.plus(16),
+                    });
+                    textOffer.at(i).assert({
+                        center: cards.at(i).center,
+                        top: textModel.at(i).bottom,
+                    });
+                }
+            });
+
+            it('more link is hidden', function () {
+                var link = frame.get('#card-group .sc-cards__show');
+                link.assert({
+                    rendered: false
+                })
+            });
+
+            it('3 cars are shown on top, 3 cars arein the middle, 2 are on the bottom', function () {
                 var cardList = frame.get('#card-group .sc-cards__list');
                 var firstCard = frame.get('#card-group .sc-card:first-of-type');
-                var secondCard = frame.get('#card-group .sc-card:nth-child(2)');
                 var thirdCard = frame.get('#card-group .sc-card:nth-child(3)');
                 var forthCard = frame.get('#card-group .sc-card:nth-child(4)');
+                var sixthCard = frame.get('#card-group .sc-card:nth-child(6)');
+                var seventhCard = frame.get('#card-group .sc-card:nth-child(7)');
+                var eighthCard = frame.get('#card-group .sc-card:nth-child(8)');
 
                 firstCard.assert({
-                    top: cardList.top
-                });
-                secondCard.assert({
                     top: cardList.top,
-                    left: firstCard.right,
-                    right: thirdCard.left
+                    left: cardList.left
                 });
                 thirdCard.assert({
                     top: cardList.top,
-                    left: secondCard.right
+                    right: cardList.right
                 });
                 forthCard.assert({
-                    top: secondCard.bottom,
-                    center: cardList.center
+                    top: firstCard.bottom,
+                    left: cardList.left
+                });
+                sixthCard.assert({
+                    top: thirdCard.bottom,
+                    right: cardList.right
+                });
+                seventhCard.assert({
+                    top: forthCard.bottom,
+                    right: cardList.center
+                });
+                eighthCard.assert({
+                    top: forthCard.bottom,
+                    left: cardList.center
                 });
             });
         } else if (720 <= browserWidth) {
-            it('cars are shown in one row', function () {
-                var cardList = frame.get('#card-group .sc-cards__list');
-                var firstCard = frame.get('#card-group .sc-card:first-of-type');
-                var secondCard = frame.get('#card-group .sc-card:nth-child(2)');
-                var thirdCard = frame.get('#card-group .sc-card:nth-child(3)');
-                var forthCard = frame.get('#card-group .sc-card:nth-child(4)');
+            it('car element text is centered', function () {
+                var cards = frame.getAll('#card-group .sc-card__image');
+                var textModel = frame.getAll('#card-group .sc-card__label div:first-child');
+                var textOffer = frame.getAll('#card-group .sc-card__label div:nth-child(2)');
 
-                firstCard.assert({
-                    top: cardList.top
+                assert(cards.length() > 0, 'we have no cards on page');
+                assert(textModel.length() > 0, 'we have no model texts on page');
+                assert(textOffer.length() > 0, 'we have no offer texts on page');
+                for (var i = 0; i < cards.length() - 1; i++) {
+                    textModel.at(i).assert({
+                        center: cards.at(i).center,
+                        top: cards.at(i).bottom.plus(16),
+                    });
+                    textOffer.at(i).assert({
+                        center: cards.at(i).center,
+                        top: textModel.at(i).bottom,
+                    });
+                }
+            });
+
+            it('more link is hidden', function () {
+                var link = frame.get('#card-group .sc-cards__show');
+                link.assert({
+                    rendered: false
+                })
+            });
+
+            it('cars are shown in two rows', function () {
+                // do we need to test this case
+                // var cardList = frame.get('#card-group .sc-cards__list');
+                // var firstCard = frame.get('#card-group .sc-card:first-of-type');
+                // var thirdCard = frame.get('#card-group .sc-card:nth-child(3)');
+                // var fifthCard = frame.get('#card-group .sc-card:nth-child(5)');
+                // var sixthCard = frame.get('#card-group .sc-card:nth-child(6)');
+                // var seventhCard = frame.get('#card-group .sc-card:nth-child(7)');
+                // var eighthCard = frame.get('#card-group .sc-card:nth-child(8)');
+
+                // firstCard.assert({
+                //     top: cardList.top,
+                //     left: cardList.left
+                // });
+                // thirdCard.assert({
+                //     top: cardList.top,
+                //     center: cardList.center
+                // });
+                // fifthCard.assert({
+                //     top: cardList.top,
+                //     right: cardList.right
+                // });
+                // sixthCard.assert({
+                //     top: firstCard.bottom
+                // });
+                // seventhCard.assert({
+                //     top: firstCard.bottom,
+                //     center: cardList.center
+                // });
+                // eighthCard.assert({
+                //     top: firstCard.bottom
+                // });
+            });
+        }
+    });
+
+    describe('Card group {INTERACTION}', function () {
+        if (browserWidth < 640) {
+
+            beforeEach(function () {
+                link = frame.get('#card-group .sc-cards__show').toDomElement();
+            })
+
+            afterEach(function (done) {
+                helper.reload(frame, done)
+            })
+
+            it('shows hidden cards on click', function () {
+                helper.click(link);
+                var cardList = frame.get('#card-group .sc-cards__list');
+                var forthCard = frame.get('#card-group .sc-card:nth-child(4)');
+                var fifthCard = frame.get('#card-group .sc-card:nth-child(5)');
+                var sixthCard = frame.get('#card-group .sc-card:nth-child(6)');
+
+                fifthCard.assert({
+                    rendered: true,
+                    top: forthCard.bottom,
+                    left: cardList.left
                 });
-                secondCard.assert({
-                    top: cardList.top,
-                    left: firstCard.right,
-                    right: thirdCard.left
-                });
-                thirdCard.assert({
-                    top: cardList.top,
-                    left: secondCard.right,
-                    right: forthCard.left
-                });
-                forthCard.assert({
-                    top: cardList.top
-                });
+                sixthCard.assert({
+                    rendered: true,
+                    top: forthCard.bottom,
+                    right: cardList.right
+                })
             });
         }
     });
