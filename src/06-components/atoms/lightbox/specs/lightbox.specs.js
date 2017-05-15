105749206de2fcc045db3a6fc808f9fe4e8d1a83
Travis CI <#AS24-Web-Experience-Team-ds@scout24.com>
2017-05-15 14:26:23 +0000

    Release

--- src/06-components/atoms/lightbox/specs/lightbox.specs.js
+++ src/06-components/atoms/lightbox/specs/lightbox.specs.js
@@ -1,60 +1,60 @@
 module.exports = function (frame, assert, browserWidth, helper) {
-    // describe('Lightbox', function () {
-    //     var trigger;
-    //     var lightbox;
+    describe('Lightbox', function () {
+        var trigger;
+        var lightbox;
 
-    //     beforeEach(function () {
-    //         lightbox = frame.get('.sc-lightbox-container');
-    //         trigger = frame.get('.sc-lightbox-open').toDomElement();
-    //     })
+        beforeEach(function () {
+            lightbox = frame.get('.sc-lightbox-container');
+            trigger = frame.get('.sc-lightbox-open').toDomElement();
+        })
 
-    //     afterEach(function (done) {
-    //         helper.reload(frame, done)
-    //     })
+        afterEach(function (done) {
+            helper.reload(frame, done)
+        })
 
-    //     it('Open lightbox', function () {
-    //         helper.click(trigger);
-    //         assert.notEqual(lightbox.getRawStyle('display'), 'none', 'should be shown');
-    //     });
+        it('Open lightbox', function () {
+            helper.click(trigger);
+            assert.notEqual(lightbox.getRawStyle('display'), 'none', 'should be shown');
+        });
 
-    //     it('Close lightbox using icon', function (done) {
-    //         helper.click(trigger);
-    //         var lightboxCloseIcon = frame.get('.sc-lightbox-container .sc-lightbox-close').toDomElement();
-    //         helper.click(lightboxCloseIcon);
-    //         setTimeout(function () {
-    //             assert.equal(lightbox.getRawStyle('display'), 'none', 'should not be shown');
-    //             done();
-    //         }, 250); //wait for fadeOut
-    //     });
+        it('Close lightbox using icon', function (done) {
+            helper.click(trigger);
+            var lightboxCloseIcon = frame.get('.sc-lightbox-container .sc-lightbox-close').toDomElement();
+            helper.click(lightboxCloseIcon);
+            setTimeout(function () {
+                assert.equal(lightbox.getRawStyle('display'), 'none', 'should not be shown');
+                done();
+            }, 250); //wait for fadeOut
+        });
 
-    //     it('Close lightbox using button', function (done) {
-    //         helper.click(trigger);
-    //         var lightboxCloseButton = frame.get('.sc-lightbox-container .sc-lightbox-close').toDomElement();
-    //         helper.click(lightboxCloseButton);
-    //         setTimeout(function () {
-    //             assert.equal(lightbox.getRawStyle('display'), 'none', 'should not be shown');
-    //             done();
-    //         }, 250); //wait for fadeOut
-    //     });
+        it('Close lightbox using button', function (done) {
+            helper.click(trigger);
+            var lightboxCloseButton = frame.get('.sc-lightbox-container .sc-lightbox-close').toDomElement();
+            helper.click(lightboxCloseButton);
+            setTimeout(function () {
+                assert.equal(lightbox.getRawStyle('display'), 'none', 'should not be shown');
+                done();
+            }, 250); //wait for fadeOut
+        });
 
-    //     it('Close lightbox clicking on overlay', function (done) {
-    //         helper.click(trigger);
-    //         var lightboxOverlay = frame.get('.sc-overlay').toDomElement();
-    //         helper.click(lightboxOverlay);
-    //         setTimeout(function () {
-    //             assert.equal(lightbox.getRawStyle('display'), 'none', 'should not be shown');
-    //             done();
-    //         }, 250); //wait for fadeOut
-    //     });
+        it('Close lightbox clicking on overlay', function (done) {
+            helper.click(trigger);
+            var lightboxOverlay = frame.get('.sc-overlay').toDomElement();
+            helper.click(lightboxOverlay);
+            setTimeout(function () {
+                assert.equal(lightbox.getRawStyle('display'), 'none', 'should not be shown');
+                done();
+            }, 250); //wait for fadeOut
+        });
 
-    //     it('Lightbox stays open if click is inside of it', function (done) {
-    //         helper.click(trigger);
-    //         var lightboxInput = frame.get('.sc-lightbox-container input').toDomElement();
-    //         helper.click(lightboxInput);
-    //         setTimeout(function () {
-    //             assert.notEqual(lightbox.getRawStyle('display'), 'none', 'should be shown');
-    //             done();
-    //         }, 250); //wait for fadeOut
-    //     });
-    // });
+        it('Lightbox stays open if click is inside of it', function (done) {
+            helper.click(trigger);
+            var lightboxInput = frame.get('.sc-lightbox-container input').toDomElement();
+            helper.click(lightboxInput);
+            setTimeout(function () {
+                assert.notEqual(lightbox.getRawStyle('display'), 'none', 'should be shown');
+                done();
+            }, 250); //wait for fadeOut
+        });
+    });
 };
