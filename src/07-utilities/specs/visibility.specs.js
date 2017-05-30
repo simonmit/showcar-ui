8b45b3453de118796cc7275b6cb0bd3492bbb1f9
meckert <mathias.eckert@autoscout24.com>
2017-05-30 21:51:04 +0200

    added new visibility classes

---
+++ src/07-utilities/specs/visibility.specs.js
@@ -0,0 +1,60 @@
+// Not yet working
+// module.exports = function (frame, assert, browserWidth, helper) {
+//     describe('Visibility classes', function () {
+        // var visibleS;
+        //
+        // beforeEach(function () {
+        //     visibleS = frame.get('#visibility .sc-visible--s');
+        // });
+        //
+        // afterEach(function (done) {
+        //     helper.reload(frame, done)
+        // });
+        //
+        // it('Visible S class', function () {
+        //     it('is visible from S view', function () {
+        //         visibleS.assert({
+        //             rendered: true
+        //         });
+        //     });
+        // });
+
+        // it('Visible S class', function () {
+        //     if (browserWidth > 320) {
+        //         it('is visible from S view', function () {
+        //             visibleS.assert({
+        //                 rendered: true
+        //             });
+        //         });
+        //     }
+        // });
+
+        // it('Hidden S class', function () {
+        //     if (browserWidth > 320) {
+        //         it('is hidden from S view', function () {
+        //             hiddenS.assert({
+        //                 rendered: false
+        //             });
+        //         });
+        //     }
+        // });
+
+        // it('Hidden M class', function () {
+        //     var hiddenEl = frame.get('#hiddenM');
+        //
+        //     if (browserWidth > 768) {
+        //         it('is hidden from M view', function () {
+        //             hiddenEl.assert({
+        //                 rendered: false
+        //             });
+        //         });
+        //     } else {
+        //         it('is visible on S view', function () {
+        //             hiddenEl.assert({
+        //                 rendered: true
+        //             });
+        //         });
+        //     }
+        // });
+//     });
+// };
