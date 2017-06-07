b9264320ce3ef485defe55dda47bb7fa754eda89
Andrei Solovev <andrei.solovev@scout24.com>
2017-06-07 14:35:06 +0200

    check for sc-lightbox__overlay

--- src/06-components/atoms/lightbox/lightbox.js
+++ src/06-components/atoms/lightbox/lightbox.js
@@ -48,6 +48,9 @@ export default function (tagName) {
     }
 
     const show = lb => {
+        if(document.querySelector('.sc-lightbox__overlay')){
+            return;
+        }
         lb.overlay = document.createElement('div');
         lb.overlay.classList.add('sc-lightbox__overlay');
         lb.overlay.classList.add('sc-overlay'); //remove
