a3da3b650fdef491b4f5f8d9ae29b39ef4080020
Diana Zubova <dzubova@autoscout24.com>
2017-06-09 14:36:14 +0200

    added visibility test

--- src/07-utilities/specs/visibility.specs.js
+++ src/07-utilities/specs/visibility.specs.js
@@ -1,5 +1,5 @@
 // Not yet working
-module.exports = function (frame, assert, browserWidth, helper) {
+module.exports = function (frame, assert, browserWidth) {
     // describe('Visibility classes', function () {
     //     var visibleS;
     //
@@ -57,4 +57,59 @@ module.exports = function (frame, assert, browserWidth, helper) {
     //         }
     //     });
     // });
+
+    describe('Visibility classes', () => {
+        let visibleS, visibleM, visibleL, visibleXL;
+        let hiddenS, hiddenM, hiddenL, hiddenXL, hiddenAll;
+
+        beforeEach(() => {
+            visibleS = frame.get('#visibility .sc-visible-at-s');
+            visibleM = frame.get('#visibility .sc-visible-at-m');
+            visibleL = frame.get('#visibility .sc-visible-at-l');
+            visibleXL = frame.get('#visibility .sc-visible-at-xl');
+            hiddenS = frame.get('#visibility .sc-hidden-at-s');
+            hiddenM = frame.get('#visibility .sc-hidden-at-m');
+            hiddenL = frame.get('#visibility .sc-hidden-at-l');
+            hiddenXL = frame.get('#visibility .sc-hidden-at-xl');
+            hiddenAll = frame.get('#visibility .sc-hidden');
+        });
+
+        if (browserWidth < 767) {
+            it('shown correctly on S view', (done) => {
+                visibleS.assert({ rendered: false });
+                visibleM.assert({ rendered: true });
+                visibleL.assert({ rendered: true });
+                visibleXL.assert({ rendered: true });
+                hiddenS.assert({ rendered: false });
+                hiddenM.assert({ rendered: true });
+                hiddenL.assert({ rendered: true });
+                hiddenXL.assert({ rendered: true });
+                hiddenAll.assert({ rendered: true });
+            });
+        } else if (browserWidth >= 768 && browserWidth < 1023) {
+            it('shown correctly on M view', (done) => {
+                visibleS.assert({ rendered: true });
+                visibleM.assert({ rendered: false });
+                visibleL.assert({ rendered: true });
+                visibleXL.assert({ rendered: true });
+                hiddenS.assert({ rendered: true });
+                hiddenM.assert({ rendered: false });
+                hiddenL.assert({ rendered: true });
+                hiddenXL.assert({ rendered: true });
+                hiddenAll.assert({ rendered: true });
+            });
+        } else {
+            it('shown correctly on L and XL view', (done) => {
+                visibleS.assert({ rendered: true });
+                visibleM.assert({ rendered: false });
+                visibleL.assert({ rendered: true });
+                visibleXL.assert({ rendered: true });
+                hiddenS.assert({ rendered: true });
+                hiddenM.assert({ rendered: false });
+                hiddenL.assert({ rendered: true });
+                hiddenXL.assert({ rendered: true });
+                hiddenAll.assert({ rendered: true });
+            });
+        }
+    });
 };
