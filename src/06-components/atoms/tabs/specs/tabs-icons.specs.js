fbd7cf0d44038ba79f885cc3f4e400a21248282e
Diana Zubova <dzubova@autoscout24.com>
2017-06-12 15:47:38 +0200

    cleaned up tabs with icons

---
+++ src/06-components/atoms/tabs/specs/tabs-icons.specs.js
@@ -0,0 +1,51 @@
+module.exports = (frame, assert, browserWidth, helper) => {
+    describe('Tabs with icons', () => {
+        let tabAuto;
+        let tabMoto;
+        let contentAuto;
+        let contentMoto;
+
+        beforeEach(() => {
+            tabAuto = frame.get("#tabs-icons .sc-tab--with-icon[data-section='search-auto']");
+            tabMoto = frame.get("#tabs-icons .sc-tab--with-icon[data-section='search-moto']");
+            contentAuto = frame.get("#tabs-icons .sc-tab-content[data-section='search-auto']");
+            contentMoto = frame.get("#tabs-icons .sc-tab-content[data-section='search-moto']");
+        })
+
+        afterEach(done => {
+            helper.reload(frame, done)
+        })
+
+        it('Click on auto tabs shows auto', () => {
+            helper.click(tabAuto.toDomElement());
+            assert.include(contentAuto.toDomElement().innerText, 'Auto', 'contains');
+            contentAuto.assert({
+                rendered: true
+            })
+            tabAuto.assert({
+                height: 45
+            })
+        });
+
+        it('Click on moto tabs shows moto', () => {
+            helper.click(tabMoto.toDomElement());
+            assert.include(contentMoto.toDomElement().innerText, 'Moto', 'contains');
+
+            // moto is active
+            contentMoto.assert({
+                rendered: true
+            })
+            tabMoto.assert({
+                height: 45
+            })
+
+            // auto is inactive
+            contentAuto.assert({
+                rendered: false
+            })
+            tabAuto.assert({
+                height: 36
+            })
+        });
+    });
+};
