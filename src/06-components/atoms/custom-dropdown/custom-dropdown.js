3da94c1f4d2d9510ff9ac0cb8f49395286eafeef
Travis CI <#AS24-Web-Experience-Team-ds@scout24.com>
2017-06-13 10:14:27 +0000

    Release

--- src/06-components/atoms/custom-dropdown/custom-dropdown.js
+++ src/06-components/atoms/custom-dropdown/custom-dropdown.js
@@ -1,40 +1,40 @@
 import registerElement from '../../../07-utilities/helpers.js';
 
-export default function(tagName) {
-    function createdCallback() {
-        let el = $(this);
-        let titleElement = el.find('p');
-        let defaultTitle = titleElement.text();
+export default function (tagName) {
+    function attachedCallback() {
+        const el = this;
+        if (! el.hasAttribute('checkboxgroup')) return;
 
-        el.removeClass('sc-open');
-        // TODO: rewrite to click
-        el.on('touchstart, mousedown', (e) => {
+        el.addEventListener('click', (e) => {
             e.stopPropagation();
         });
 
-        if (null === el.attr('checkboxgroup')) {
-            return;
-        }
+        const titleElement = el.querySelector('p');
+        const defaultTitle = titleElement.innerHTML;
 
-        if (this.hasAttribute('checkboxgroup')) {
-            el.find('[type=checkbox]').addClass('sc-input');
-            let updateCaption = () => {
-                let checkboxes = el.find(':checked');
-                let texts = [];
-                checkboxes.filter(':checked').forEach((element) => {
-                    texts.push(element.nextElementSibling.innerHTML);
+        el.classList.remove('sc-open'); // TODO check do we need this?
+
+        const checkboxes = el.querySelectorAll('[type=checkbox]'); // TODO check do we need this?
+        Array.from(checkboxes).forEach((checkbox) => { // TODO check do we need this?
+            checkbox.classList.add('sc-input'); // TODO check do we need this?
+        }); // TODO check do we need this?
+
+        const updateCaption = () => {
+            const checkboxesChecked = el.querySelectorAll(':checked');
+
+            const texts = Array.from(checkboxesChecked)
+                .map((checkboxChecked) => {
+                    return checkboxChecked.nextElementSibling.innerHTML;
                 });
 
-                let title = texts.join(', ') || defaultTitle;
-                titleElement.html(title);
-            };
+            const title = texts.join(', ') || defaultTitle;
+            titleElement.innerHTML = title;
+        };
 
-            el.on('change', updateCaption);
-            updateCaption();
-        }
-    }
+        el.addEventListener('change', updateCaption);
+        
+        updateCaption();
 
-    function attachedCallback() {
         if (this.hasAttribute('checkboxgroup')) {
             this.querySelector('p').addEventListener('click', () => {
                 closeAllDropdowns(this);
@@ -45,9 +45,6 @@ export default function(tagName) {
         }
     }
 
-    /**
-     * @param {HTMLElement} exceptThisOne
-     */
     function closeAllDropdowns(exceptThisOne) {
         return () => Array.from(document.querySelectorAll(tagName))
             .filter((cdd) => cdd !== exceptThisOne)
@@ -57,12 +54,11 @@ export default function(tagName) {
     function attachEventListeners() {
         // this should only be done at most once
         // when the first of this element gets attached
-        document.addEventListener('mousedown', closeAllDropdowns(this));
+        document.addEventListener('click', closeAllDropdowns(this));
         attachEventListeners = () => {}; // eslint-disable-line no-func-assign
     }
 
     registerElement({
-        createdCallback,
         attachedCallback,
         tagName
     });
