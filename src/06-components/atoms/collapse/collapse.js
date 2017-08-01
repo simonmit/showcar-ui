6d35352aaa71388c11065da0e1f62e66360e036b
Andrei Solovev <andrei.solovev@scout24.com>
2017-08-01 16:19:52 +0200

    bump --skip-tests

--- src/06-components/atoms/collapse/collapse.js
+++ src/06-components/atoms/collapse/collapse.js
@@ -2,14 +2,15 @@ export default () => {
     const handleClick = (collapsable) => {
         let targetAttr = collapsable.getAttribute('data-target');
         let targets = document.querySelectorAll(targetAttr);
-    
+
         Array.prototype.forEach.call(targets, (target) => {
             target.classList.toggle('in');
         });
-    
+
         let event = new CustomEvent('collapse', {
             bubbles: true
         });
+        
         document.dispatchEvent(event);
     };
 
