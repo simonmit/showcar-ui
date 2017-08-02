c3e250d770142360f56495417ee23fabec99d01c
bdadam <me@bdadam.com>
2017-08-03 00:16:30 +0200

    shadow traffic for new error collector

--- src/js/inline-js/js-error-collector.js
+++ src/js/inline-js/js-error-collector.js
@@ -1,45 +1,26 @@
-(function(navigator) {
+(function(navigator, location) {
     'use strict';
 
-    function getPostUrl() {
-        var url = location.host;
-        var devUrl = 'https://dev-js-error-logger.infinity.eu-west-1.s24cloud.net/log';
-        var prodUrl = 'https://5q1eumnb90.execute-api.eu-west-1.amazonaws.com/default/event';
-
-        if (url.indexOf('dev-www.') > -1) {
-            return devUrl;
-        } else if (url.indexOf('www.') > -1) {
-            return prodUrl;
-        }
-
-        return '';
-    }
-
-    function postError(data) {
+    function postError(url, data) {
         var http = new XMLHttpRequest();
-        var url = getPostUrl();
 
-        if (url !== '') {
-            http.open('POST', url, true);
-            http.setRequestHeader('Content-Type', 'application/json');
+        if (url !== "") {
+            http.open("POST", url, true);
+            http.setRequestHeader("Content-Type", "application/json");
             http.send(data);
         }
     }
 
-    function extractPagePathFromPath() {
-        var pathArray = location.pathname.split('/');
-        return pathArray[1];
-    }
-
     window.onerror = function(errorMsg, sourceUrl, lineNumber, column, errorObj) {
-        if (errorMsg.indexOf('Script error') > -1) {
+        
+        if (location.host.indexOf('www.autoscout24') < 0) {
             return;
         }
-        
+
         var data = {
             httpUri: location.href,
             httpReferrer: document.referrer,
-            pagePath: extractPagePathFromPath(),
+            pagePath: location.pathname.split('/'),
             jsSrc: sourceUrl || '',
             jsLine: lineNumber || '',
             jsColumn: column || '',
@@ -48,7 +29,16 @@
             errorStacktrace: errorObj ? errorObj.stack : '',
             errorMessage: errorMsg || ''
         };
+        
+        var devUrl = "https://2w6tdi5ifg.execute-api.eu-west-1.amazonaws.com/default/event";
+        var prodUrl = "https://5q1eumnb90.execute-api.eu-west-1.amazonaws.com/default/event";
 
-        postError(JSON.stringify(data));
+        var url = (location.host.indexOf("dev-www.") > -1) ? devUrl : prodUrl;
+        postError(url, JSON.stringify(data));
+
+        var devShadowUrl = 'https://dev-js-error-logger.infinity.eu-west-1.s24cloud.net/log';
+        var prodShadowUrl = 'https://js-error-logger.infinity.eu-west-1.s24cloud.net/log';
+        var shadowUrl = (url.indexOf("dev-www.") > -1) ? devShadowUrl : prodShadowUrl;
+        postError(url, JSON.stringify(data));
     };
-})(navigator);
+})(navigator, location);
