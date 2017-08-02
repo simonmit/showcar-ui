11a9194fedeff940198dca8b7fb8dd89cd2e864b
Diana Zubova <dzubova@autoscout24.com>
2017-08-02 16:13:41 +0200

    fix build

--- travis_release.sh
+++ travis_release.sh
@@ -30,7 +30,7 @@ git add . -A
 #checking for files to commit, if exists then commit. If not go further
 if [ -n "$(git status --porcelain)" ]; then
 	git commit -am "Release"
-	npm version patch
+	# npm version patch
 	git push origin $RELEASE_BRANCH --follow-tags
 fi
 
