bd6cd70824d09d89ad31276e16801eb2db9552c7
Travis CI <#AS24-Web-Experience-Team-ds@scout24.com>
2017-08-03 13:24:45 +0000

    Release

--- src/js/inline-js/js-error-collector.js
+++ src/js/inline-js/js-error-collector.js
@@ -10,6 +10,10 @@
 
     window.onerror = function(errorMsg, sourceUrl, lineNumber, column, errorObj) {
         
+        if (errorMsg.indexOf('Script error') > -1) {
+            return;
+        }
+        
         if (location.host.indexOf('www.autoscout24') < 0) {
             return;
         }
@@ -27,14 +31,8 @@
             errorMessage: errorMsg || ''
         };
         
-        var devUrl = 'https://2w6tdi5ifg.execute-api.eu-west-1.amazonaws.com/default/event';
-        var prodUrl = 'https://5q1eumnb90.execute-api.eu-west-1.amazonaws.com/default/event';
-
-        var url = (location.host.indexOf('dev-www.') > -1) ? devUrl : prodUrl;
+        var prefix = (location.host.indexOf('dev-www.') > -1) ? 'dev-' : '';
+        var url = 'https://' + prefix + 'js-error-logger.infinity.eu-west-1.s24cloud.net/log';
         postError(url, JSON.stringify(data));
-
-        // var prefix = (location.host.indexOf('dev-www.') > -1) ? 'dev-' : '';
-        // var shadowUrl = 'https://' + prefix + 'js-error-logger.infinity.eu-west-1.s24cloud.net/log';
-        // postError(shadowUrl, JSON.stringify(data));
     };
 })(navigator, location);
