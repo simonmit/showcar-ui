d5c690b464c09add99634c77b5b51c79cebe42cb
Adam Beres-Deak <me@bdadam.com>
2017-08-03 14:38:22 +0200

    Disabled error collector shadow traffic --skip-test

--- src/js/inline-js/js-error-collector.js
+++ src/js/inline-js/js-error-collector.js
@@ -33,8 +33,8 @@
         var url = (location.host.indexOf('dev-www.') > -1) ? devUrl : prodUrl;
         postError(url, JSON.stringify(data));
 
-        var prefix = (location.host.indexOf('dev-www.') > -1) ? 'dev-' : '';
-        var shadowUrl = 'https://' + prefix + 'js-error-logger.infinity.eu-west-1.s24cloud.net/log';
-        postError(shadowUrl, JSON.stringify(data));
+        // var prefix = (location.host.indexOf('dev-www.') > -1) ? 'dev-' : '';
+        // var shadowUrl = 'https://' + prefix + 'js-error-logger.infinity.eu-west-1.s24cloud.net/log';
+        // postError(shadowUrl, JSON.stringify(data));
     };
 })(navigator, location);
