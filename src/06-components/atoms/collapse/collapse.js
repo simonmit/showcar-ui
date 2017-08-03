95f75a7528d6e6223b5f82762029f13841585f4a
Travis CI <#AS24-Web-Experience-Team-ds@scout24.com>
2017-08-03 11:46:04 +0000

    Release

--- src/06-components/atoms/collapse/collapse.js
+++ src/06-components/atoms/collapse/collapse.js
@@ -1,34 +1,22 @@
 export default () => {
-    const handleClick = (collapsable) => {
-        let targetAttr = collapsable.getAttribute('data-target');
-        let targets = document.querySelectorAll(targetAttr);
-
-        Array.prototype.forEach.call(targets, (target) => {
-            target.classList.toggle('in');
-        });
-
-        let event = new CustomEvent('collapse', {
-            bubbles: true
-        });
-        document.dispatchEvent(event);
-    };
-
-    document.addEventListener('click', (e => {
-        let target = e.target;
-
-        while(target) {
-            if (target.getAttribute && target.getAttribute('data-toggle') === 'sc-collapse') {
-                return handleClick(target);
-            }
-            target = target.parentNode;
-        }
-    }));
-
-    function reInit() {
+    function init() {
         Array.prototype.forEach.call(document.querySelectorAll('[data-toggle="sc-collapse"]'), (collapsable) => {
-            collapsable.addEventListener('click', () => {handleClick(collapsable);});
+            collapsable.onclick = () => {
+                let targetAttr = collapsable.getAttribute('data-target');
+                let targets = document.querySelectorAll(targetAttr);
+
+                Array.prototype.forEach.call(targets, (target) => {
+                    target.classList.toggle('in');
+                });
+
+                let event = new CustomEvent('collapse', {
+                    bubbles: true
+                });
+                document.dispatchEvent(event);
+            };
         });
     }
 
-    document.addEventListener('as24-collapse:update', reInit);
+    init();
+    document.addEventListener('as24-collapse:update', init);
 };
