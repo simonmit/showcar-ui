63139170cb52a818e9c2cbd28998ea623b44bf09
Diana Zubova <diana.zubova@scout24.com>
2017-11-10 17:49:58 +0100

    added on-demand-load for google-map

--- src/06-components/atoms/google-map/google-map.js
+++ src/06-components/atoms/google-map/google-map.js
@@ -1,6 +1,19 @@
 import registerElement from '../../../07-utilities/helpers.js';
 
 export default function (tagName) {
+    let intersectionObserver;
+    if(window.IntersectionObserver) {
+        intersectionObserver = new IntersectionObserver(entries => { // eslint-disable-line
+            // If intersectionRatio is 0, the sentinel is out of view
+            // and we do not need to do anything.
+            if (entries[0].intersectionRatio <= 0) {
+                return;
+            }
+
+            loadMap(entries[0].target);
+        });
+    }
+
     function loadMap(el) {
         const publicKey = el.getAttribute('apikey');
         const address = el.getAttribute('query');
@@ -13,25 +26,26 @@ export default function (tagName) {
         map.setAttribute('allowfullscreen', true);
         map.setAttribute('title', 'google-map');
         el.appendChild(map);
+
+        if(window.IntersectionObserver) {
+            intersectionObserver.unobserve(el);
+        } else {
+            return;
+        }
     }
 
     function attachedCallback() {
         const sentinel = this;
 
-        window.addEventListener('load', function() {
-            intersectionObserver.observe(sentinel);
-        }, false);
-    }
-
-    var intersectionObserver = new IntersectionObserver(entries => { // eslint-disable-line
-        // If intersectionRatio is 0, the sentinel is out of view
-        // and we do not need to do anything.
-        if (entries[0].intersectionRatio <= 0) {
-            return;
+        if(window.IntersectionObserver) {
+            console.log('here');
+            window.addEventListener('load', function() {
+                intersectionObserver.observe(sentinel);
+            }, false);
+        } else {
+            loadMap(sentinel);
         }
-
-        loadMap(entries[0].target);
-    });
+    }
 
     registerElement({
         attachedCallback,
