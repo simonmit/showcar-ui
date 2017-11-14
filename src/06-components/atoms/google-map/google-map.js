0290b32a876252be353f8b6819a78a5fa99d5a8d
Diana Zubova <diana.zubova@scout24.com>
2017-11-14 16:43:38 +0100

    restructured js in google map

--- src/06-components/atoms/google-map/google-map.js
+++ src/06-components/atoms/google-map/google-map.js
@@ -1,37 +1,32 @@
 import registerElement from '../../../07-utilities/helpers.js';
 
+const loadMap = (el) => {
+    const publicKey = el.getAttribute('apikey');
+    const address = el.getAttribute('query');
+    const htmlTag = document.querySelector('html');
+    const lang = htmlTag.getAttribute('lang') || 'en';
+    const url = `https://www.google.com/maps/embed/v1/place?language=${lang}&key=${publicKey}&q=${encodeURIComponent(address)}`;
+    const map = document.createElement('iframe');
+
+    map.setAttribute('src', url);
+    map.setAttribute('allowfullscreen', true);
+    map.setAttribute('title', 'google-map');
+    el.appendChild(map);
+};
+
 export default function (tagName) {
     let intersectionObserver;
     if (window.IntersectionObserver) {
         intersectionObserver = new IntersectionObserver(entries => { // eslint-disable-line
             // If intersectionRatio is 0, the sentinel is out of view
             // and we do not need to do anything.
-            entries.forEach(entry => {
-                if (entry.intersectionRatio <= 0) {
-                    return;
-                }
-                loadMap(entry.target);
-            });
+            entries.filter(entry => entry.intersectionRatio > 0)
+                .forEach(entry => {
+                    loadMap(entry.target);
+                    intersectionObserver.unobserve(entry.target);
+                });
         });
     }
-
-    const loadMap = (el) => {
-        const publicKey = el.getAttribute('apikey');
-        const address = el.getAttribute('query');
-        const htmlTag = document.querySelector('html');
-        const lang = htmlTag.getAttribute('lang') || 'en';
-        const url = `https://www.google.com/maps/embed/v1/place?language=${lang}&key=${publicKey}&q=${encodeURIComponent(address)}`;
-        const map = document.createElement('iframe');
-
-        map.setAttribute('src', url);
-        map.setAttribute('allowfullscreen', true);
-        map.setAttribute('title', 'google-map');
-        el.appendChild(map);
-
-        if (window.IntersectionObserver) {
-            intersectionObserver.unobserve(el);
-        }
-    };
 
     function attachedCallback() {
         const sentinel = this;
