678928c01585341ad1e37104c105a695968ef211
Diana Zubova <diana.zubova@scout24.com>
2017-11-15 17:26:42 +0100

    finished styles for desktop tabs-text

--- src/06-components/atoms/tabs/tabs-text.js
+++ src/06-components/atoms/tabs/tabs-text.js
@@ -1,18 +1,18 @@
 export default () => {
     window.addEventListener('DOMContentLoaded', function () {
         Array.prototype.forEach.call(document.querySelectorAll('[data-tabs-text]'), (tabContainer) => {
-            const tabs = tabContainer.querySelectorAll('.sc-tab button');
+            const tabs = tabContainer.querySelectorAll('.sc-tab');
             Array.prototype.forEach.call(tabs, (tab) => {
                 tab.addEventListener('click', (e) => {
                     const currentActiveTab = tabContainer.querySelector('.sc-tab--with-text--active');
                     const sectionAttr = currentActiveTab.getAttribute('data-section');
                     const currentSection = tabContainer.querySelector('.sc-tabs__content[data-section=' + sectionAttr + ']');
 
-                    e.target.parentElement.classList.add('sc-tab--with-text--active');
+                    e.target.classList.add('sc-tab--with-text--active');
                     if (currentActiveTab) currentActiveTab.classList.remove('sc-tab--with-text--active');
                     if (currentSection) currentSection.classList.remove('sc-tabs__content--visible');
 
-                    const sectionAttrNew = e.target.parentElement.getAttribute('data-section');
+                    const sectionAttrNew = e.target.getAttribute('data-section');
                     const sectionNew = document.querySelector('.sc-tabs__content[data-section=' + sectionAttrNew + ']');
                     if (sectionNew) sectionNew.classList.add('sc-tabs__content--visible');
                 });
