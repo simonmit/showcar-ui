bb8cc068f6549c75816e2c5f9ddcbd32bb778e35
Diana Zubova <diana.zubova@scout24.com>
2017-11-16 11:48:39 +0100

    added event delegation to tabs with text

--- src/06-components/atoms/tabs/tabs-text.js
+++ src/06-components/atoms/tabs/tabs-text.js
@@ -1,9 +1,8 @@
 export default () => {
     window.addEventListener('DOMContentLoaded', function () {
         Array.prototype.forEach.call(document.querySelectorAll('[data-tabs-text]'), (tabContainer) => {
-            const tabs = tabContainer.querySelectorAll('.sc-tab');
-            Array.prototype.forEach.call(tabs, (tab) => {
-                tab.addEventListener('click', (e) => {
+            tabContainer.addEventListener('click', (e) => {
+                if(e.target && e.target.classList.contains('sc-tab')) {
                     const currentActiveTab = tabContainer.querySelector('.sc-tab--with-text--active');
                     const sectionAttr = currentActiveTab.getAttribute('data-section');
                     const currentSection = tabContainer.querySelector('.sc-tabs__content[data-section=' + sectionAttr + ']');
@@ -15,7 +14,7 @@ export default () => {
                     const sectionAttrNew = e.target.getAttribute('data-section');
                     const sectionNew = document.querySelector('.sc-tabs__content[data-section=' + sectionAttrNew + ']');
                     if (sectionNew) sectionNew.classList.add('sc-tabs__content--visible');
-                });
+                }
             });
         });
     });
