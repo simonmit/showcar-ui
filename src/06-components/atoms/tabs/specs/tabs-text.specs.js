708c4cef2f96345ca10ebccaf122440672bb4a35
Diana Zubova <diana.zubova@scout24.com>
2017-11-16 13:06:06 +0000

    e0e37a3 added tests to tabs-text

---
+++ src/06-components/atoms/tabs/specs/tabs-text.specs.js
@@ -0,0 +1,75 @@
+module.exports = (frame, assert, browserWidth, helper) => {
+    describe('Tabs with text', () => {
+        let firstTab;
+        let secondTab;
+        let firstContent;
+        let secondContent;
+
+        beforeEach(() => {
+            firstTab = frame.get("#tabs-text .sc-tab[data-section=Tab1]");
+            secondTab = frame.get("#tabs-text .sc-tab[data-section=Tab2]");
+            firstContent = frame.get("#tabs-text .sc-tabs__content[data-section=Tab1]");
+            secondContent = frame.get("#tabs-text .sc-tabs__content[data-section=Tab2]");
+        })
+
+        afterEach(done => {
+            helper.reload(frame, done)
+        })
+
+        if (browserWidth < 768) {
+            it('Click on first tab shows first content as accordion', () => {
+                helper.click(firstTab.toDomElement());
+                assert.include(firstContent.toDomElement().innerText, 'Tab 1', 'contains');
+
+                firstContent.assert({
+                    rendered: true,
+                    top: firstTab.bottom.plus(3),
+                    bottom: secondTab.top.minus(3)
+                })
+                secondContent.assert({
+                    rendered: false
+                })
+            });
+            it('Click on second tab shows second content as accordion', () => {
+                helper.click(secondTab.toDomElement());
+                assert.include(secondContent.toDomElement().innerText, 'Tab 2', 'contains');
+
+                firstContent.assert({
+                    rendered: false
+                })
+                secondContent.assert({
+                    rendered: true,
+                    top: secondTab.bottom
+                })
+            });
+        } else {
+            it('Click on first tab shows first content', () => {
+                helper.click(firstTab.toDomElement());
+                assert.include(firstContent.toDomElement().innerText, 'Tab 1', 'contains');
+
+                firstContent.assert({
+                    rendered: true,
+                    top: firstTab.bottom.plus(3)
+                })
+                secondContent.assert({
+                    rendered: false
+                })
+                secondTab.assert({
+                    left: firstTab.right.plus(3)
+                })
+            });
+            it('Click on second tab shows second content', () => {
+                helper.click(secondTab.toDomElement());
+                assert.include(secondContent.toDomElement().innerText, 'Tab 2', 'contains');
+
+                firstContent.assert({
+                    rendered: false
+                })
+                secondContent.assert({
+                    rendered: true,
+                    top: firstTab.bottom.plus(3)
+                })
+            });
+        }
+    });
+};
