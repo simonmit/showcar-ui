9cb94ce40b7d40ca0afe949a95428afc6b777810
Diana Zubova <diana.zubova@scout24.com>
2017-11-20 10:26:39 +0000

    84c12c3 removed scrolling from tabs

--- src/06-components/atoms/tabs/tabs-text.js
+++ src/06-components/atoms/tabs/tabs-text.js
@@ -1,26 +1,18 @@
 export default () => {
-    window.addEventListener('DOMContentLoaded', function () {
-        Array.prototype.forEach.call(document.querySelectorAll('[data-tabs-text]'), (tabContainer) => {
-            tabContainer.addEventListener('click', (e) => {
-                if(e.target && e.target.classList.contains('sc-tab')) {
-                    const currentActiveTab = tabContainer.querySelector('.sc-tab--with-text--active');
-                    const sectionAttr = currentActiveTab.getAttribute('data-section');
-                    const currentSection = tabContainer.querySelector('.sc-tabs__content[data-section=' + sectionAttr + ']');
+    document.addEventListener('click', (e) => {
+        if(e.target && e.target.classList.contains('sc-tab') && e.target.hasAttribute('data-section')) {
+            const tabContainer = e.target.parentElement;
+            const currentActiveTab = tabContainer.querySelector('.sc-tab--with-text--active');
+            const sectionAttr = currentActiveTab.getAttribute('data-section');
+            const currentSection = tabContainer.querySelector('.sc-tabs__content[data-section=' + sectionAttr + ']');
 
-                    if (currentActiveTab) currentActiveTab.classList.remove('sc-tab--with-text--active');
-                    if (currentSection) currentSection.classList.remove('sc-tabs__content--visible');
-                    e.target.classList.add('sc-tab--with-text--active');
+            if (currentActiveTab) currentActiveTab.classList.remove('sc-tab--with-text--active');
+            if (currentSection) currentSection.classList.remove('sc-tabs__content--visible');
+            e.target.classList.add('sc-tab--with-text--active');
 
-                    const sectionAttrNew = e.target.getAttribute('data-section');
-                    const sectionNew = document.querySelector('.sc-tabs__content[data-section=' + sectionAttrNew + ']');
-                    if (sectionNew) sectionNew.classList.add('sc-tabs__content--visible');
-
-                    const containerWidth = window.innerWidth
-                        || document.documentElement.clientWidth
-                        || document.body.clientWidth;
-                    if (containerWidth < 768) currentActiveTab.scrollIntoView(true);
-                }
-            });
-        });
+            const sectionAttrNew = e.target.getAttribute('data-section');
+            const sectionNew = tabContainer.querySelector('.sc-tabs__content[data-section=' + sectionAttrNew + ']');
+            if (sectionNew) sectionNew.classList.add('sc-tabs__content--visible');
+        }
     });
 };
