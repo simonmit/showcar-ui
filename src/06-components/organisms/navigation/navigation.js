e5d1bd75f7bb931e1dbccba96e72dff4e1d8f69a
Diana Zubova <diana.zubova@scout24.com>
2017-11-20 14:23:52 +0000

    aa58978 navigation doesn't intersect keyup event anymore

--- src/06-components/organisms/navigation/navigation.js
+++ src/06-components/organisms/navigation/navigation.js
@@ -1,4 +1,32 @@
 class Navigation {
+    get KEY_DOWN() {
+        return 40;
+    }
+
+    get KEY_UP() {
+        return 38;
+    }
+
+    get KEY_LEFT() {
+        return 37;
+    }
+
+    get KEY_RIGHT() {
+        return 39;
+    }
+
+    get KEY_TAB() {
+        return 9;
+    }
+
+    get KEY_RETURN() {
+        return 13;
+    }
+
+    get KEY_ESCAPE() {
+        return 27;
+    }
+
     /**
      * @param {HTMLElement} root
      */
@@ -24,6 +52,8 @@ class Navigation {
         this.rootElement.on('click', 'ul>li', $.proxy(this.toggleMenu, this));
         this.menuBtn.on('click', $.proxy(this.toggleMenu, this));
         this.document.on('click', $.proxy(this.escapeMenu, this));
+        this.document.on('keydown', $.proxy(this.onKeyDown, this));
+        this.document.on('keyup', $.proxy(this.onKeyUp, this));
     }
 
     /**
@@ -89,6 +119,117 @@ class Navigation {
      */
     escapeMenu() {
         this.activeMenu && this.menuIsOpen && this.closeMenu();
+    }
+
+    /**
+     * @param {Number} keyCode
+     * @returns {boolean}
+     */
+    isNavigationKey(keyCode) {
+        return [
+            this.KEY_DOWN,
+            this.KEY_LEFT,
+            this.KEY_RIGHT,
+            this.KEY_UP,
+            this.KEY_TAB
+        ].indexOf(keyCode) > -1;
+    }
+
+    /**
+     * Prevent scrolling
+     *
+     * @param {Object} event
+     * @returns {boolean}
+     */
+    onKeyDown(event) {
+        let keyCode = event.which;
+
+        if (this.menuIsOpen && this.isNavigationKey(keyCode)) {
+            event.preventDefault();
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param {Object} event
+     * @returns {boolean}
+     */
+    onKeyUp(event) {
+        if(this.menuIsOpen) {
+            let keyCode = event.which;
+
+            switch (keyCode) {
+                case this.KEY_ESCAPE:
+                    this.escapeMenu();
+                    break;
+                case this.KEY_DOWN:
+                    this.handleJumpDown();
+                    break;
+                case this.KEY_UP:
+                    this.handleJumpUp();
+                    break;
+                case this.KEY_TAB:
+                    event.shiftKey ? this.handleJumpLeft() : this.handleJumpRight();
+                    break;
+                case this.KEY_RIGHT:
+                    this.handleJumpRight();
+                    break;
+                case this.KEY_LEFT:
+                    this.handleJumpLeft();
+                    break;
+            }
+
+            event.preventDefault();
+            return false;
+        }
+    }
+
+    handleJumpDown() {
+        // Expand the menu if closed
+        if (false === this.menuIsOpen) {
+            this.openMenu();
+            this.selectFirstItem();
+            return;
+        }
+        let position = this.items.indexOf(this.activeItem);
+        this.items.length - 1 > position && position++;
+        this.setActiveMenuItem(this.items[position]);
+    }
+
+    handleJumpUp() {
+        if (false === this.menuIsOpen) {
+            return;
+        }
+        let position = this.items.indexOf(this.activeItem);
+        0 === position && this.closeMenu();
+        0 < position && position--;
+        this.setActiveMenuItem(this.items[position]);
+    }
+
+    handleJumpRight() {
+        if (!this.activeMenu) {
+            // quick fix when active menu doesn't exists because of any unknown reason
+            // TODO: fix it properly
+            return;
+        }
+
+        let current = this.menus.indexOf(this.activeMenu[0]);
+        let newMenuIdx = this.menus.length - 1 > current ? newMenuIdx = current + 1 : 0;
+        this.selectMenu(this.menus[newMenuIdx]);
+    }
+
+    handleJumpLeft() {
+        if (!this.activeMenu) {
+            // quick fix when active menu doesn't exists because of any unknown reason
+            // TODO: fix it properly
+            return;
+        }
+
+        let current = this.menus.indexOf(this.activeMenu[0]);
+        let newMenuIdx = (0 < current) ? current - 1 : this.menus.length - 1;
+        this.selectMenu(this.menus[newMenuIdx]);
     }
 
     /**
