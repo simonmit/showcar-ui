e98ed7818a84be6433654e68b2341d7e604ae410
Diana Zubova <diana.zubova@scout24.com>
2017-12-06 11:27:19 +0100

    typo fix

--- src/06-components/atoms/rotating-arrow/rotating-arrow.js
+++ src/06-components/atoms/rotating-arrow/rotating-arrow.js
@@ -1,10 +1,18 @@
+// export default () => {
+//     window.addEventListener('DOMContentLoaded', function () {
+//         Array.prototype.forEach.call(document.querySelectorAll('[data-toggle="arrow"]'), (arrow) => {
+//             let arrowEl = $(arrow);
+//             $(arrowEl.data('target')).on('click', () => {
+//                 arrowEl.toggleClass('open');
+//             });
+//         });
+//     }, true);
+// };
+
 export default () => {
-    window.addEventListener('DOMContentLoaded', function () {
-        Array.prototype.forEach.call(document.querySelectorAll('[data-toggle="arrow"]'), (arrow) => {
-            let arrowEl = $(arrow);
-            $(arrowEl.data('target')).on('click', () => {
-                arrowEl.toggleClass('open');
-            });
-        });
-    }, true);
+    document.addEventListener('click', (e) => {
+        if(e.target && e.target.matches && e.target.matches('[data-toggle="arrow"]')) {
+            e.target.classList.contains('open') ? e.target.classList.remove('open') : e.target.classList.add('open');
+        }
+    });
 };
--- src/06-components/atoms/stepper/stepper.js
+++ src/06-components/atoms/stepper/stepper.js
@@ -1,6 +1,43 @@
 export default () => {
-    window.addEventListener('DOMContentLoaded', function() {
-        Array.prototype.forEach.call(document.querySelectorAll('.sc-stepper'), (stepperContainer) => {
+    // window.addEventListener('DOMContentLoaded', function() {
+    //     Array.prototype.forEach.call(document.querySelectorAll('.sc-stepper'), (stepperContainer) => {
+    //         const stepperEl = stepperContainer.querySelector('.sc-stepper-input');
+    //         const decrementEl = stepperContainer.querySelector('.sc-stepper-button-decrement');
+    //         const incrementEl = stepperContainer.querySelector('.sc-stepper-button-increment');
+    //         const minValue = parseInt(stepperEl.getAttribute('min')) || 0;
+    //         const maxValue = parseInt(stepperEl.getAttribute('max')) || 100;
+
+    //         // To make sure we can also calculate with the value the user typed in
+    //         const getCurrentValue = () => {
+    //             return stepperEl.value !== '' ? parseInt(stepperEl.value, 10) : 0;
+    //         };
+
+    //         var currentValue = getCurrentValue();
+
+    //         const increment = () => {
+    //             currentValue = getCurrentValue();
+
+    //             if (currentValue < maxValue) {
+    //                 stepperEl.value = ++currentValue;
+    //             }
+    //         };
+
+    //         const decrement = () => {
+    //             currentValue = getCurrentValue();
+
+    //             if (currentValue > minValue) {
+    //                 stepperEl.value = --currentValue;
+    //             }
+    //         };
+
+    //         decrementEl.addEventListener('click', decrement);
+    //         incrementEl.addEventListener('click', increment);
+    //     });
+    // });
+
+    document.addEventListener('click', (e) => {
+        if (e.target && e.target.matches && e.target.matches('.sc-stepper')) {
+            const stepperContainer = e.target;
             const stepperEl = stepperContainer.querySelector('.sc-stepper-input');
             const decrementEl = stepperContainer.querySelector('.sc-stepper-button-decrement');
             const incrementEl = stepperContainer.querySelector('.sc-stepper-button-increment');
@@ -32,6 +69,6 @@ export default () => {
 
             decrementEl.addEventListener('click', decrement);
             incrementEl.addEventListener('click', increment);
-        });
+        }
     });
 };
