b9379b02ff90b11ae2c1de01e122b70f5857de51
Pascal Jeschke <pj@pascaljeschke.de>
2018-09-18 15:24:06 +0200

    Fixed typo

--- src/06-components/atoms/disruptor-small/specs/disruptor-small.specs.js
+++ src/06-components/atoms/disruptor-small/specs/disruptor-small.specs.js
@@ -1,7 +1,7 @@
 module.exports = (frame) => {
     describe('Disruptor Small', () => {
         it('Disruptor Small is rendered', () => {
-            const disruptor-small = frame.getAll('.sc-disruptor-small');
+            const disruptorSmall = frame.getAll('.sc-disruptor-small');
 
             disruptor.at(0).assert({
                 rendered: true
