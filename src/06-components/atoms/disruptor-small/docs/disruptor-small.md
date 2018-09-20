e8cd047e9cf1a19207aae22f7c21482570e5f135
Diana Zubova <diana.zubova@scout24.com>
2018-09-20 13:46:55 +0000

    67b46b7 4.3.22

--- src/06-components/atoms/disruptor-small/docs/disruptor-small.md
+++ src/06-components/atoms/disruptor-small/docs/disruptor-small.md
@@ -1,2 +1,2 @@
-<h2>Disruptor Small<span class="status review">REVIEW</span></h2>
-<p>This Disruptor Small is currently used in the main navigation only.</p>
+<h2>Disruptor Small<span class="status approved">APPROVED</span></h2>
+<p>The Disruptor Small is currently used in the main navigation only.</p>
--- src/06-components/atoms/disruptor-small/specs/disruptor-small.specs.js
+++ src/06-components/atoms/disruptor-small/specs/disruptor-small.specs.js
@@ -1,9 +1,9 @@
 module.exports = (frame) => {
     describe('Disruptor Small', () => {
         it('Disruptor Small is rendered', () => {
-            const disruptor-small = frame.getAll('.sc-disruptor-small');
+            const disruptorSmall = frame.getAll('.sc-disruptor-small');
 
-            disruptor.at(0).assert({
+            disruptorSmall.at(0).assert({
                 rendered: true
             });
         });
