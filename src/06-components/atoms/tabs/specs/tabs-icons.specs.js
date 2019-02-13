a0ac30d225b4c78d870212b6bb83dfe238c99eb6
Diana Zubova <diana.zubova@scout24.com>
2019-02-13 08:49:18 +0000

    17c496c 4.3.65

--- src/06-components/atoms/tabs/specs/tabs-icons.specs.js
+++ src/06-components/atoms/tabs/specs/tabs-icons.specs.js
@@ -1,5 +1,5 @@
 module.exports = (frame, assert, browserWidth, helper) => {
-    describe('Tabs with icons', () => {
+    describe("Tabs with icons", () => {
         let tabAuto;
         let tabMoto;
         let contentAuto;
@@ -10,60 +10,60 @@ module.exports = (frame, assert, browserWidth, helper) => {
             tabMoto = frame.get("#tabs-icons .sc-tab--with-icon[data-section=tab-moto]");
             contentAuto = frame.get("#tabs-icons .sc-tabs__content[data-section=tab-auto]");
             contentMoto = frame.get("#tabs-icons .sc-tabs__content[data-section=tab-moto]");
-        })
+        });
 
         afterEach(done => {
-            helper.reload(frame, done)
-        })
+            helper.reload(frame, done);
+        });
 
-        it('Click on auto tabs shows auto', () => {
+        it("Click on auto tabs shows auto", () => {
             helper.click(tabAuto.toDomElement());
-            assert.include(contentAuto.toDomElement().innerText, 'Auto', 'contains');
+            assert.include(contentAuto.toDomElement().innerText, "Auto", "contains");
             contentAuto.assert({
                 rendered: true
-            })
+            });
 
             if (browserWidth < 510) {
                 tabAuto.assert({
-                    height: 45
-                })
+                    height: 41
+                });
             } else {
                 tabAuto.assert({
                     height: 53
-                })
+                });
             }
         });
 
-        it('Click on moto tabs shows moto', () => {
+        it("Click on moto tabs shows moto", () => {
             helper.click(tabMoto.toDomElement());
-            assert.include(contentMoto.toDomElement().innerText, 'Moto', 'contains');
+            assert.include(contentMoto.toDomElement().innerText, "Moto", "contains");
 
             // moto is active
             contentMoto.assert({
                 rendered: true
-            })
+            });
             if (browserWidth < 510) {
                 tabMoto.assert({
-                    height: 45
-                })
+                    height: 41
+                });
             } else {
                 tabMoto.assert({
                     height: 53
-                })
+                });
             }
 
             // auto is inactive
             contentAuto.assert({
                 rendered: false
-            })
+            });
             if (browserWidth < 510) {
                 tabAuto.assert({
                     height: 36
-                })
+                });
             } else {
                 tabAuto.assert({
                     height: 48
-                })
+                });
             }
         });
     });
