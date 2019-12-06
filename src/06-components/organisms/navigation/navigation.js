67f57d1e921f3fdfcb7c72040bca9f4fa9b587ef
Diana Zubova <diana.zubova@scout24.com>
2019-12-06 16:42:40 +0100

    fixed tab navigation for new header

--- src/06-components/organisms/navigation/navigation.js
+++ src/06-components/organisms/navigation/navigation.js
@@ -264,7 +264,7 @@ class Navigation {
 }
 
 export default function () {
-    let navigationElement = document.querySelector('header[role=navigation]'),
+    let navigationElement = document.querySelector('header[role=navigation]:not(.sc-navigation-v2)'),
         navigation = null;
     if (navigationElement) {
         navigation = new Navigation(navigationElement);
