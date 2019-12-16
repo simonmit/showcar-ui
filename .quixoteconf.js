72af12ab7cc89a32b8d96d1b8e1dee6f05ccb88d
Diana Zubova <diana.zubova@scout24.com>
2019-12-16 12:43:24 +0100

    adapt width to not hit the timeout

--- .quixoteconf.js
+++ .quixoteconf.js
@@ -1,6 +1,7 @@
 var config = {
     url: 'http://localhost:9876/test/',   // karma url with port
-    width: [320, 768, 1024],
+    // width: [320, 768, 1024],
+    width: [320, 1024],
     filesSet: [
         require.context('./src/', true, /^((?!tag|notification|navigation|tooltip).)*(specs.js)$/),
     ]
